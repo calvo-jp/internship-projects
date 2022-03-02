c2920ab719ca30222684482205304e4ebb688914
jp calvo <calvojp92@gmail.com>
2022-03-02 15:04:03 +0800

    wiw

--- src/pages/ssr/pokemons/index.tsx
+++ src/pages/ssr/pokemons/index.tsx
@@ -1,31 +1,21 @@
-import { Box, Container, Flex, Grid, GridItem } from "@chakra-ui/react";
+import { Box, Grid, GridItem } from "@chakra-ui/react";
 import Header from "components/Header";
 import Pagination from "components/Pagination";
 import PokemonCard from "components/PokemonCard";
-import { GetStaticProps, NextPage } from "next";
+import { GetServerSideProps, NextPage } from "next";
 import Head from "next/head";
+import { useRouter } from "next/router";
 import IPokemon from "types/pokemon";
-import calcOffset from "utils/calcOffset";
 import getPokemons from "utils/getPokemons";
 
 interface Props {
   data: Awaited<ReturnType<typeof getPokemons>>;
 }
 
-interface Params {
-  page: string;
-  [key: string]: string;
-}
-
-export const getServerSideProps: GetStaticProps<Props, Params> = async ({
-  params,
-}) => {
-  const page = params?.page;
-  const limit = 30;
-  const offset = calcOffset({ page, limit });
-
-  const endpoint = `${process.env.API_BASE_URL}?limit=${limit}&offset=${offset}`;
-  const data = await getPokemons(endpoint);
+export const getServerSideProps: GetServerSideProps<Props> = async (
+  context
+) => {
+  const data = await getPokemons(context.query);
 
   return {
     props: {
@@ -35,7 +25,19 @@ export const getServerSideProps: GetStaticProps<Props, Params> = async ({
 };
 
 const Pokemons: NextPage<Props> = ({ data }) => {
-  const next = () => {};
+  const router = useRouter();
+
+  const next = () => {
+    if (!data.hasNext) return;
+
+    router.push({
+      pathname: "/ssr/pokemons",
+      query: {
+        page: data.page + 1,
+        pageSize: data.pageSize,
+      },
+    });
+  };
 
   const prev = () => {};
 
@@ -48,7 +50,7 @@ const Pokemons: NextPage<Props> = ({ data }) => {
       <Header />
 
       <Box as="main" p={4}>
-        <PokemonList pokemons={data.results} />
+        <PokemonList pokemons={data.rows} />
         <Pagination onPrev={prev} onNext={next} />
       </Box>
     </>
--- src/utils/getPokemons.ts
+++ src/utils/getPokemons.ts
@@ -6,43 +6,82 @@ interface IResult {
   url: string;
 }
 
+type StringOrNull = string | null;
+
 interface IPaginated {
   count: number;
   results: IResult[];
-  next: string | null;
-  previous: string | null;
+  next: StringOrNull;
+  previous: StringOrNull;
 }
 
-/**
- *
- * @param url - the pokedex api url to fetch
- *
- * @example
- * ```javascript
- * const url = "https://pokeapi.co/api/v2/pokemon?limit=12";
- * const data = await getPokemons(url);
- * ...
- * ```
- *
- */
-const getPokemons = async (url: string) => {
-  const response = await fetch(url);
+type StringOrNumber = string | number;
+
+interface Params {
+  /** page number or index */
+  page?: StringOrNumber;
+  /** page size or limit */
+  pageSize?: StringOrNumber;
+}
+
+/** default page size */
+const PAGESIZE = 30;
+
+const getPokemons = async ({ page = 1, pageSize = PAGESIZE }: Params = {}) => {
+  page = parseIntOrCoalesceIfNanOrZero(page, 1);
+  pageSize = parseIntOrCoalesceIfNanOrZero(pageSize, PAGESIZE);
+  pageSize = inRangeCoalesce(pageSize, PAGESIZE, 100);
+
+  const offset = (page - 1) * pageSize;
+
+  const params = new URLSearchParams();
+  params.append("limit", pageSize.toString());
+  params.append("offset", offset.toString());
+  const query = params.toString();
+
+  const response = await fetch(`${process.env.API_BASE_URL}?${query}`);
   const data: IPaginated = await response.json();
 
   const promises = data.results.map(({ url }) => fetch(url));
   const results = await Promise.allSettled(promises);
-  const pokemons: IPokemon[] = [];
+  const rows: IPokemon[] = [];
 
   for (const result of results) {
     if (result.status === "fulfilled") {
       const parsed = await result.value.json();
       const pokemon = normalizePokemonObject(parsed);
 
-      pokemons.push(pokemon);
+      rows.push(pokemon);
     }
   }
 
-  return { ...data, results: pokemons };
+  return {
+    page,
+    pageSize,
+    rows,
+    totalRows: data.count,
+    hasNext: !!data.next,
+    hasPrevious: !!data.previous,
+  };
+};
+
+const inRangeCoalesce = (subject: number, min: number, max: number) => {
+  return subject <= max && subject >= min ? subject : min;
+};
+
+const parseIntOrCoalesceIfNanOrZero = <_, T>(
+  subject: undefined | string | number,
+  defaultValue: T
+) => {
+  // undefined
+  if (!subject) return defaultValue;
+
+  // number <= 0
+  if (typeof subject === "number") return subject > 0 ? subject : defaultValue;
+
+  // string
+  const value = parseInt(subject);
+  return !Number.isNaN(value) && value > 0 ? value : defaultValue;
 };
 
 export default getPokemons;
