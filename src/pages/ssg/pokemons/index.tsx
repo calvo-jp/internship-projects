f1bde6185845a833c7e96ad2806ca92971f619d2
jp calvo <calvojp92@gmail.com>
2022-03-03 07:48:08 +0800

    updated pageSize from 36 to 120

--- src/pages/ssg/pokemons/index.tsx
+++ src/pages/ssg/pokemons/index.tsx
@@ -14,7 +14,7 @@ interface Props {
 }
 
 export const getStaticProps: GetStaticProps<Props> = async () => {
-  const data = await getPokemons({ pageSize: 36 });
+  const data = await getPokemons({ pageSize: 120 });
 
   return {
     revalidate: 60 * 60 * 24 * 3, // 3days
