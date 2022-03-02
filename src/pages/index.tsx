05afc6abb370d7d1a205f2b5e9337bd60a8f3440
jp calvo <calvojp92@gmail.com>
2022-03-02 10:30:38 +0800

    partial landing

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,24 @@
-import { Box, Link } from "@chakra-ui/react";
+import { Box, Link, Stack } from "@chakra-ui/react";
 import NextLink from "next/link";
+import { PropsWithChildren } from "react";
 
 export default function Landing() {
   return (
-    <Box>
-      <NextLink passHref href="/ssr/pokemons">
-        <Link>SSR</Link>
-      </NextLink>
+    <Box
+      minHeight="100vh"
+      display="flex"
+      alignItems="center"
+      justifyContent="center"
+    >
+      <Stack>
+        <NextLink passHref href="/ssr/pokemons">
+          <Link fontSize="3xl">SSR</Link>
+        </NextLink>
 
-      <NextLink passHref href="/ssg/pokemons">
-        <Link>SSG</Link>
-      </NextLink>
+        <NextLink passHref href="/ssg/pokemons">
+          <Link fontSize="3xl">SSG</Link>
+        </NextLink>
+      </Stack>
     </Box>
   );
 }
