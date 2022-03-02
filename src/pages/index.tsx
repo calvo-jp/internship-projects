33c39661f47b83a5a596520f30d59480abd3d6c8
jp calvo <calvojp92@gmail.com>
2022-03-03 07:45:27 +0800

    updated landing

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { Box, Link, Stack, Text } from "@chakra-ui/react";
+import { Box, Stack, Text } from "@chakra-ui/react";
 import Head from "next/head";
 import NextLink from "next/link";
 import { PropsWithChildren } from "react";
@@ -16,26 +16,87 @@ export default function Landing() {
         alignItems="center"
         justifyContent="center"
       >
-        <Stack>
-          <NextLink passHref href="/ssr/pokemons">
-            <Link>
-              <Text fontSize="3xl" lineHeight={1}>
-                SSR
-              </Text>
-              <Text fontSize="sm">Server Side Rendering</Text>
-            </Link>
-          </NextLink>
+        <Stack spacing={8} as="main">
+          <Heading />
 
-          <NextLink passHref href="/ssg/pokemons">
-            <Link>
-              <Text fontSize="3xl" lineHeight={1}>
-                SSR
-              </Text>
-              <Text fontSize="sm">Server Side Generation</Text>
-            </Link>
-          </NextLink>
+          <Stack direction="row" spacing={4}>
+            <CustomLink
+              href="/ssr/pokemons"
+              label="SSR"
+              helperText="Server Side Rendering"
+            />
+
+            <CustomLink
+              href="/ssg/pokemons"
+              label="SSG"
+              helperText="Static Site Generation"
+            />
+          </Stack>
+
+          <Box as="footer">
+            <Text align="center" color="gray.500">
+              ⚡ Powered by pokeapi
+            </Text>
+          </Box>
         </Stack>
       </Box>
     </>
   );
 }
+
+interface CustomLinkProps {
+  href: string;
+  label: string;
+  helperText: string;
+}
+
+const CustomLink = ({ href, label, helperText }: CustomLinkProps) => {
+  return (
+    <NextLink passHref href={href}>
+      <Box
+        as="a"
+        textAlign="center"
+        border={1}
+        borderStyle="solid"
+        borderColor="gray.300"
+        py={2}
+        px={4}
+        rounded="sm"
+        _hover={{
+          borderColor: "orange.300",
+        }}
+      >
+        <Text fontSize="5xl" lineHeight={1}>
+          {label}
+        </Text>
+        <Text fontSize="sm" color="gray.600">
+          {helperText}
+        </Text>
+      </Box>
+    </NextLink>
+  );
+};
+
+const Heading = () => {
+  return (
+    <Box>
+      <Text
+        as="h1"
+        w="fit-content"
+        mx="auto"
+        fontSize={64}
+        fontWeight={700}
+        lineHeight={1}
+        backgroundClip="text"
+        fill="transparent"
+        bgGradient="linear(to right, orange.400, yellow.500)"
+      >
+        POKEDEX
+      </Text>
+
+      <Text as="p" textAlign="center" fontSize="sm" color="gray.500">
+        Next SSG and SSR Demo
+      </Text>
+    </Box>
+  );
+};
