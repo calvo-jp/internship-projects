5bf9ddd8b10c19eb6555e93b4104ba2153479901
jp calvo <calvojp92@gmail.com>
2022-03-03 13:27:13 +0800

    error fallback testing

---
+++ src/components/errors/Internal.tsx
@@ -0,0 +1,11 @@
+import { Box, Text } from "@chakra-ui/react";
+
+const InternalError = () => {
+  return (
+    <Box>
+      <Text>Something went wrong</Text>
+    </Box>
+  );
+};
+
+export default InternalError;
