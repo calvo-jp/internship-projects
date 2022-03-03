8db4a6197b6ec0be8aafe6e7912f85843d2bcc0b
jp calvo <calvojp92@gmail.com>
2022-03-03 13:27:31 +0800

    404 page init

---
+++ src/pages/404.tsx
@@ -0,0 +1,14 @@
+import { Box } from "@chakra-ui/react";
+import Head from "next/head";
+
+export default function NotFound() {
+  return (
+    <>
+      <Head>
+        <title>Error 404 | Page not found</title>
+      </Head>
+
+      <Box></Box>
+    </>
+  );
+}
