bf392a0ae7c4e0b2461123290cc3178473faed5e
jp calvo <calvojp92@gmail.com>
2022-03-03 08:10:45 +0800

    added image in header as bg

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,18 +1,22 @@
 import { Box, Text } from "@chakra-ui/react";
+import pokeball from "assets/pokeball.png";
+import Image from "next/image";
 import Link from "next/link";
 
 const Header = () => {
   return (
     <Box
       as="header"
-      minHeight={200}
-      bgGradient="linear(to right, orange.400, yellow.400)"
+      height={180}
+      bgGradient="linear(to right, orange.400, yellow.500)"
       roundedBottom="3xl"
       color="white"
       display="flex"
       flexDirection="column"
       alignItems="center"
       justifyContent="center"
+      position="relative"
+      overflow="hidden"
     >
       <Link passHref href="/">
         <a>
@@ -23,6 +27,17 @@ const Header = () => {
       </Link>
 
       <Text as="p">Powered by pokeapi</Text>
+
+      <Box
+        position="absolute"
+        height={150}
+        width={150}
+        right={-8}
+        bottom={-8}
+        opacity=".4"
+      >
+        <Image src={pokeball} alt="" layout="fill" />
+      </Box>
     </Box>
   );
 };
