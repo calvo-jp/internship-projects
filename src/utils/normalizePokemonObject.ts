f335fa2827a27de96bedd5de32a91f0562362629
jp calvo <calvojp92@gmail.com>
2022-03-03 09:44:39 +0800

    fixed 500 error bug by adding default image and experience

--- src/utils/normalizePokemonObject.ts
+++ src/utils/normalizePokemonObject.ts
@@ -3,13 +3,34 @@ import IPokemon from "types/pokemon";
 type Dict = Record<string, any>;
 
 export default function normalizePokemonObject(data: Dict): IPokemon {
+  // Checking missing keys in debug mode
+  if (process.env.NODE_ENV === "development") {
+    const keys: (keyof IPokemon)[] = [
+      "id",
+      "name",
+      "image",
+      "types",
+      "abilities",
+      "moves",
+      "stats",
+      "experience",
+      "weight",
+      "height",
+    ];
+
+    for (const key of keys) {
+      if (!data[key]) console.error("Missing " + key);
+    }
+  }
+
   try {
     return {
       id: data.id,
       name: data.name,
       image:
         data.sprites.other.dream_world.front_default ||
-        data.sprites.front_default,
+        data.sprites.front_default ||
+        defaultImage,
       types: data.types.map((type: Dict) => type.type.name),
       abilities: data.abilities.map((ability: Dict) =>
         ability.ability.name.replace(/-/g, " ")
@@ -21,7 +42,7 @@ export default function normalizePokemonObject(data: Dict): IPokemon {
         value: stat.base_stat,
         name: stat.stat.name.replace(/-/g, " "),
       })),
-      experience: data.base_experience,
+      experience: data.base_experience || 0,
       weight: data.weight,
       height: data.height,
       __original__: data,
@@ -30,3 +51,5 @@ export default function normalizePokemonObject(data: Dict): IPokemon {
     throw new Error("Something happened while parsing object");
   }
 }
+
+const defaultImage = "/pokeball.png";
