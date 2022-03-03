5d8d72d3376012a382aff8f2f8f12625df373c94
jp calvo <calvojp92@gmail.com>
2022-03-03 08:48:39 +0800

    changed pageSize to 60

--- src/pages/ssg/pokemons/[id].tsx
+++ src/pages/ssg/pokemons/[id].tsx
@@ -17,7 +17,7 @@ interface Params {
 }
 
 export const getStaticPaths: GetStaticPaths<Params> = async () => {
-  const data = await getPokemons();
+  const data = await getPokemons({ pageSize: 60 });
 
   return {
     paths: data.rows.map(({ id }) => ({ params: { id: id.toString() } })),
