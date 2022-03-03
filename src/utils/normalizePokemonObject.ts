18a025f059f23132bf03b7937e1303e7a40ae3f6
jp calvo <calvojp92@gmail.com>
2022-03-04 07:52:17 +0800

    refactor

--- src/utils/normalizePokemonObject.ts
+++ src/utils/normalizePokemonObject.ts
@@ -3,34 +3,30 @@ import IPokemon from "types/pokemon";
 type Dict = Record<string, any>;
 
 export default function normalizePokemonObject(data: Dict): IPokemon {
-  try {
-    return {
-      id: data.id,
-      name: data.name,
-      image: unsafeCoalesce(
-        data.sprites.other.dream_world.front_default,
-        data.sprites.other.home.front_default,
-        data.sprites.front_default,
-        defaultImage
-      ),
-      types: data.types.map((type: Dict) => type.type.name),
-      abilities: data.abilities.map((ability: Dict) =>
-        normalizeKebab(ability.ability.name)
-      ),
-      moves: data.moves
-        .map((move: Dict) => normalizeKebab(move.move.name))
-        .slice(0, 10),
-      stats: data.stats.map((stat: Dict) => ({
-        value: stat.base_stat,
-        name: normalizeKebab(stat.stat.name),
-      })),
-      experience: unsafeCoalesce(data.base_experience, 0),
-      weight: data.weight,
-      height: data.height,
-    };
-  } catch (e) {
-    throw new Error("Something happened while parsing object");
-  }
+  return {
+    id: data.id,
+    name: data.name,
+    image: unsafeCoalesce(
+      data.sprites.other.dream_world.front_default,
+      data.sprites.other.home.front_default,
+      data.sprites.front_default,
+      defaultImage
+    ),
+    types: data.types.map((type: Dict) => type.type.name),
+    abilities: data.abilities.map((ability: Dict) =>
+      normalizeKebab(ability.ability.name)
+    ),
+    moves: data.moves
+      .map((move: Dict) => normalizeKebab(move.move.name))
+      .slice(0, 10),
+    stats: data.stats.map((stat: Dict) => ({
+      value: stat.base_stat,
+      name: normalizeKebab(stat.stat.name),
+    })),
+    experience: unsafeCoalesce(data.base_experience, 0),
+    weight: data.weight,
+    height: data.height,
+  };
 }
 
 const unsafeCoalesce = (...args: any): any => {
@@ -38,7 +34,7 @@ const unsafeCoalesce = (...args: any): any => {
 };
 
 const normalizeKebab = (subject: string) => {
-  return subject.replace(/-/g, " ").trim();
+  return subject.replace(/[a-z0-9]-[a-z0-9]/gi, (m) => m.replace(/-/, " "));
 };
 
 const defaultImage = "/pokeball.png";
