c083d054ba2dd97f90904f82042057d03d8975fb
jp calvo <calvojp92@gmail.com>
2022-03-06 10:24:21 +0800

    refactor

--- src/utils/isObject.ts
+++ src/utils/isObject.ts
@@ -1,8 +1,7 @@
-const isObject = (subject: any): subject is Record<string, any> => {
-  return (
-    Object(subject) === subject &&
-    Object.prototype.toString.call(subject) === "[object Object]"
-  );
-};
+const tag = "[object Object]";
+
+const isObject = (subject: any): subject is Record<string, any> =>
+  Object(subject) === subject &&
+  Object.prototype.toString.call(subject) === tag;
 
 export default isObject;
