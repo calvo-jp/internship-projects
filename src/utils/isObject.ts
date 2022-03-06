ccac35a55c84b39641f03fe6a3d3970704652a67
jp calvo <calvojp92@gmail.com>
2022-03-06 19:47:27 +0800

    changed type to unknown

--- src/utils/isObject.ts
+++ src/utils/isObject.ts
@@ -1,6 +1,6 @@
 const tag = "[object Object]";
 
-const isObject = (subject: any): subject is Record<string, any> =>
+const isObject = (subject: unknown): subject is Record<string, any> =>
   Object(subject) === subject &&
   Object.prototype.toString.call(subject) === tag;
 
