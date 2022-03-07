e5c5152bd56167ea9706b853e1c2c0ccde76aaa6
jp calvo <calvojp92@gmail.com>
2022-03-07 22:29:01 +0800

    refactor

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,34 +1,23 @@
-import { Box, Text, VStack, Wrap, WrapItem } from "@chakra-ui/react";
+import { Box, Icon, Text, VStack, Wrap, WrapItem } from "@chakra-ui/react";
 import FacebookIcon from "./icons/Facebook";
 import InstagramIcon from "./icons/Instagram";
 import LinkedInIcon from "./icons/Linkedin";
 import TwitterIcon from "./icons/Twitter";
 
 const Footer = () => {
+  const icons = [FacebookIcon, InstagramIcon, TwitterIcon, LinkedInIcon];
+
   return (
     <Box as="footer" p={8} mt={16}>
       <VStack spacing={4}>
         <Wrap spacing={4}>
-          <WrapItem>
-            <a href="#">
-              <FacebookIcon />
-            </a>
-          </WrapItem>
-          <WrapItem>
-            <a href="#">
-              <InstagramIcon />
-            </a>
-          </WrapItem>
-          <WrapItem>
-            <a href="#">
-              <TwitterIcon />
-            </a>
-          </WrapItem>
-          <WrapItem>
-            <a href="#">
-              <LinkedInIcon />
-            </a>
-          </WrapItem>
+          {icons.map((SVGIcon, index) => (
+            <WrapItem key={index}>
+              <a href="#" target="_blank" rel="noreferrer">
+                <Icon as={SVGIcon} w={30} h={30} />
+              </a>
+            </WrapItem>
+          ))}
         </Wrap>
 
         <Text fontSize="14px">Copyright ©2020 All rights reserved</Text>
