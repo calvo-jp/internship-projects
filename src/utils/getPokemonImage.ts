2adb8e2eed3114672a2823ee50c894b3f4a6e99e
jp calvo <calvojp92@gmail.com>
2022-03-11 07:59:22 +0800

    verify before sending a cdn

--- src/utils/getPokemonImage.ts
+++ src/utils/getPokemonImage.ts
@@ -1,7 +1,26 @@
 import { CorePokemonData } from "types/CorePokemonData";
 
 const getPokemonImage = (pokemon: CorePokemonData) => {
-  return `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${pokemon.id}.svg`;
+  const sprites = pokemon.sprites.at(0);
+
+  if (!!sprites) {
+    try {
+      const obj = JSON.parse(sprites.sprites);
+
+      const img =
+        obj.other.dream_world.front_default ||
+        obj.other.home.front_default ||
+        obj.front_default;
+
+      if (img) return img;
+    } catch {}
+  }
+
+  return getCdn(pokemon.id);
+};
+
+const getCdn = (id: number | string) => {
+  return `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${id}.svg`;
 };
 
 export default getPokemonImage;
