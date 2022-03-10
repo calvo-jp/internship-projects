d3c68dc5638cfdc3bea8dff3c71ab581904502c0
jp calvo <calvojp92@gmail.com>
2022-03-10 18:36:13 +0800

    updates

--- src/pages/ssg/pokemons/index.tsx
+++ src/pages/ssg/pokemons/index.tsx
@@ -1,11 +1,14 @@
+import { useLazyQuery } from "@apollo/client";
 import { Stack } from "@chakra-ui/react";
 import Header from "components/Header";
+import InfiniteScroll from "components/InfiniteScroll";
 import PokemonList from "components/PokemonList";
 import ScrollToTopButton from "components/ScrollToTopButton";
 import apolloClient from "config/apollo/client";
 import { GET_POKEMONS } from "graphql/queries";
 import { GetStaticProps, NextPage } from "next";
 import Head from "next/head";
+import { useState } from "react";
 import { GetPokemons, GetPokemonsVariables } from "types/GetPokemons";
 
 interface Props {
@@ -13,12 +16,12 @@ interface Props {
 }
 
 export const getStaticProps: GetStaticProps<Props> = async () => {
-  // pre-render only 50 rows,
+  // pre-render only 30 rows,
   // other data will be fetched on the client-side
   const { data } = await apolloClient.query<GetPokemons, GetPokemonsVariables>({
     query: GET_POKEMONS,
     variables: {
-      limit: 500,
+      limit: 30,
     },
   });
 
@@ -31,6 +34,30 @@ export const getStaticProps: GetStaticProps<Props> = async () => {
 };
 
 const Pokemons: NextPage<Props> = ({ data }) => {
+  const [rows, setRows] = useState(data);
+
+  const [, { loading, fetchMore }] = useLazyQuery<
+    GetPokemons,
+    GetPokemonsVariables
+  >(GET_POKEMONS, {
+    variables: {
+      offset: data.length,
+      limit: 30,
+    },
+    notifyOnNetworkStatusChange: true,
+    onCompleted({ pokemons }) {
+      setRows([...data, ...pokemons]);
+    },
+  });
+
+  const next = async () => {
+    await fetchMore({
+      variables: {
+        offset: rows.length,
+      },
+    });
+  };
+
   return (
     <>
       <Head>
@@ -40,9 +67,12 @@ const Pokemons: NextPage<Props> = ({ data }) => {
       <Header />
 
       <Stack as="main" p={{ base: 2, md: 4 }} spacing={{ base: 2, md: 4 }}>
-        <PokemonList pokemons={data} isSSG />
+        <PokemonList pokemons={rows} isSSG />
+
+        {loading && <div>Loading...</div>}
       </Stack>
 
+      <InfiniteScroll callback={next} paused={loading} />
       <ScrollToTopButton />
     </>
   );
