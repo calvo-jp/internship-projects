1f196d3672d98bc6a4732f81a97b54b2fecda5ec
jp calvo <calvojp92@gmail.com>
2022-03-13 09:07:26 +0800

    Initial next auth setup

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -11,13 +11,16 @@ import "@fontsource/ubuntu/400.css";
 import "@fontsource/ubuntu/500.css";
 import "@fontsource/ubuntu/700.css";
 import theme from "config/theme";
+import { SessionProvider } from "next-auth/react";
 import type { AppProps } from "next/app";
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
-    <ChakraProvider theme={theme}>
-      <Component {...pageProps} />
-    </ChakraProvider>
+    <SessionProvider session={pageProps.session}>
+      <ChakraProvider theme={theme}>
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </SessionProvider>
   );
 };
 
