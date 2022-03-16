6259beafed1597dc934c77f4421ef7e8fc6aa4de
jp calvo <calvojp92@gmail.com>
2022-03-16 11:39:12 +0800

    added custom fonts

--- pokedex/src/pages/_app.tsx
+++ pokedex/src/pages/_app.tsx
@@ -1,5 +1,14 @@
 import { ApolloProvider } from "@apollo/client";
 import { ChakraProvider } from "@chakra-ui/react";
+import "@fontsource/inter/100.css";
+import "@fontsource/inter/200.css";
+import "@fontsource/inter/300.css";
+import "@fontsource/inter/400.css";
+import "@fontsource/inter/500.css";
+import "@fontsource/inter/600.css";
+import "@fontsource/inter/700.css";
+import "@fontsource/inter/800.css";
+import "@fontsource/inter/900.css";
 import apolloClient from "config/apollo/client";
 import theme from "config/theme";
 import type { AppProps } from "next/app";
