d6b766766b949fdcef591e5e03841bc6a51c52f6
jp calvo <calvojp92@gmail.com>
2022-03-16 20:58:29 +0800

    theme updates

--- pokedex/src/config/theme.ts
+++ pokedex/src/config/theme.ts
@@ -7,7 +7,18 @@ const theme = extendTheme({
   },
   colors: {
     brand: {
+      primary: "#FFD12D",
       tertiary: "#3F81E4",
+      gray: {
+        50: "#F7FAFC",
+        400: "#A0AEC0",
+        500: "#718096",
+        800: "#1F2937",
+        900: "#111827",
+      },
+      red: {
+        500: "#EF4444",
+      },
     },
   },
 });
