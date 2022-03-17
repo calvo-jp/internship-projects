d81ab63b579130898ff77de1dede8259520c1d10
jp calvo <calvojp92@gmail.com>
2022-03-17 17:41:50 +0800

    added variants

--- pokedex/src/components/widgets/card/Tag.tsx
+++ pokedex/src/components/widgets/card/Tag.tsx
@@ -1,14 +1,41 @@
-import { Tag } from "@chakra-ui/react";
+import { Tag, TagProps } from "@chakra-ui/react";
+import clsx from "clsx";
 import { PropsWithChildren } from "react";
 
-const CardTag = ({ children }: PropsWithChildren<{}>) => {
+type BaseProps = Omit<TagProps, "variant">;
+
+interface CardTagProps {
+  variant: "error" | "success" | "info";
+}
+
+const CardTag = ({
+  variant,
+  children,
+  ...props
+}: PropsWithChildren<CardTagProps & BaseProps>) => {
   return (
     <Tag
       py={2}
       px={8}
-      color="brand.red.700"
-      bgColor="brand.red.50"
+      border="1px"
+      color={clsx({
+        "brand.red.700": variant === "error",
+        "brand.blue.700": variant === "info",
+        "brand.green.700": variant === "success",
+      })}
+      bgColor={clsx({
+        "brand.red.50": variant === "error",
+        "brand.blue.50": variant === "info",
+        "brand.green.50": variant === "success",
+      })}
+      borderColor={clsx({
+        "brand.red.200": variant === "error",
+        "brand.blue.200": variant === "info",
+        "brand.green.200": variant === "success",
+      })}
+      fontFamily="'Public Sans', sans-serif"
       fontSize="xs"
+      {...props}
     >
       {children}
     </Tag>
