7c8b6198c02448c4967093122e7555df15923dd3
jp calvo <calvojp92@gmail.com>
2022-03-17 15:49:16 +0800

    removed unnecessary Fragment

--- pokedex/src/components/layouts/homepage/index.tsx
+++ pokedex/src/components/layouts/homepage/index.tsx
@@ -5,19 +5,17 @@ import Background from "./Background";
 
 const HomepageLayout = ({ children }: PropsWithChildren<{}>) => {
   return (
-    <>
-      <Flex minH="100vh" direction="column">
-        <Header />
+    <Flex minH="100vh" direction="column">
+      <Header />
 
-        <Box position="relative" flexGrow="1" w="full">
-          <Box as="main" position="relative" zIndex="1">
-            {children}
-          </Box>
-
-          <Background />
+      <Box position="relative" flexGrow="1" w="full">
+        <Box as="main" position="relative" zIndex="1">
+          {children}
         </Box>
-      </Flex>
-    </>
+
+        <Background />
+      </Box>
+    </Flex>
   );
 };
 
