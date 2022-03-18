f7dca478afd3001e28434b26a94d1aaa4b5142b1
jp calvo <calvojp92@gmail.com>
2022-03-18 13:47:36 +0800

    now using valx

--- pokedex/src/components/widgets/card/CardTag.tsx
+++ pokedex/src/components/widgets/card/CardTag.tsx
@@ -1,6 +1,6 @@
 import { Tag, TagProps } from "@chakra-ui/react";
-import clsx from "clsx";
 import { PropsWithChildren } from "react";
+import valx from "utils/valx";
 
 type BaseProps = Omit<TagProps, "variant">;
 
@@ -18,17 +18,17 @@ const CardTag = ({
       py={2}
       px={8}
       border="1px"
-      color={clsx({
+      color={valx({
         "brand.red.700": variant === "error",
         "brand.blue.700": variant === "info",
         "brand.green.700": variant === "success",
       })}
-      bgColor={clsx({
+      bgColor={valx({
         "brand.red.50": variant === "error",
         "brand.blue.50": variant === "info",
         "brand.green.50": variant === "success",
       })}
-      borderColor={clsx({
+      borderColor={valx({
         "brand.red.200": variant === "error",
         "brand.blue.200": variant === "info",
         "brand.green.200": variant === "success",
