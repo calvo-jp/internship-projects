169a7320b99aeb7ede0e9a0c62c2d45acdc87c82
jp calvo <calvojp92@gmail.com>
2022-03-18 17:15:43 +0800

    added alert widget

---
+++ pokedex/src/components/widgets/Alert.tsx
@@ -0,0 +1,55 @@
+import {
+  Alert as ChakraAlert,
+  AlertDescription,
+  AlertIcon,
+  AlertProps as ChakraAlertProps,
+  CloseButton,
+  Collapse,
+} from "@chakra-ui/react";
+
+type BaseProps = Omit<ChakraAlertProps, "in" | "status">;
+
+interface AlertProps {
+  open?: boolean;
+  message?: string;
+  onClose?: () => void;
+  variant?: ChakraAlertProps["status"];
+}
+
+/**
+ *
+ * Abstraction of Chakra's `Alert` with more sensible props.
+ * This component is wrapped inside a `Collapse` and is hidden by default.
+ *
+ * @example
+ * <Alert
+ *    mt={4}
+ *    mb={2}
+ *    open={!!error}
+ *    variant="error"
+ *    message={error}
+ *    onClose={() => setError("")}
+ * />
+ *
+ */
+const Alert = ({
+  open,
+  message,
+  onClose,
+  variant,
+  ...props
+}: AlertProps & BaseProps) => {
+  return (
+    <Collapse in={open} unmountOnExit>
+      <ChakraAlert status={variant} rounded="sm" shadow="sm" {...props}>
+        <AlertIcon />
+        <AlertDescription flexGrow="1" ml={2}>
+          {message}
+        </AlertDescription>
+        <CloseButton onClick={onClose} />
+      </ChakraAlert>
+    </Collapse>
+  );
+};
+
+export default Alert;
