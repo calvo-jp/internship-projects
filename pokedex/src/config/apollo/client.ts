ad2f163a16b10fa8d9ae265179493a241466419f
jp calvo <calvojp92@gmail.com>
2022-03-18 17:16:29 +0800

    updated env var

--- pokedex/src/config/apollo/client.ts
+++ pokedex/src/config/apollo/client.ts
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 const apolloClient = new ApolloClient({
-  uri: process.env.NEXT_PUBLIC_GRAPHQL_ENDPOINT,
+  uri: process.env.NEXT_PUBLIC_GRAPHQL_ENDPOINT_MAIN,
   cache: new InMemoryCache(),
 });
 
