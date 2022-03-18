debaab4ab9a6c9c04e3a836cc4c8bc9a49c0448e
jp calvo <calvojp92@gmail.com>
2022-03-18 21:59:27 +0800

    refactor

--- pokedex/src/pages/pokemons/index.tsx
+++ pokedex/src/pages/pokemons/index.tsx
@@ -15,18 +15,17 @@ import {
   MenuItem,
   MenuList,
   SimpleGrid,
-  Table,
-  Tbody,
-  Td,
   Text,
-  Thead,
-  Tr,
   Wrap,
   WrapItem,
 } from "@chakra-ui/react";
 import { ChevronLeftIcon, ChevronRightIcon } from "@heroicons/react/outline";
 import { FilterIcon, ViewGridIcon, ViewListIcon } from "@heroicons/react/solid";
 import HomepageLayout from "components/layouts/homepage";
+import GridTable from "components/widgets/grid-table";
+import GridTableCell from "components/widgets/grid-table/GridTableCell";
+import GridTableHeading from "components/widgets/grid-table/GridTableHeading";
+import GridTableRow from "components/widgets/grid-table/GridTableRow";
 import useStore from "hooks/useStore";
 import Head from "next/head";
 import NextImage from "next/image";
@@ -41,148 +40,39 @@ const Pokemons = () => {
       </Head>
 
       <HomepageLayout>
-        <Box
-          maxW="container.lg"
-          mx="auto"
-          p={{ base: 4, md: 8, lg: 12 }}
-          bgColor="transparent"
-        >
-          <HStack justify="space-between">
-            <Heading
-              fontSize="2xl"
-              color="brand.gray.100"
-              fontWeight="semibold"
-            >
-              Choose Pokemon
-            </Heading>
+        <Box p={{ base: 4, md: 8, lg: 12 }} maxW="container.lg" mx="auto">
+          <Section1 />
 
-            <Toolbar />
-          </HStack>
-
-          <Box mt={{ base: 6, lg: 12 }}>
-            <Items />
-          </Box>
-
-          <Box mt={{ base: 2, lg: 4 }}>
+          <Flex
+            as="section"
+            mt={{ base: 6, lg: 12 }}
+            gap={{ base: 2, lg: 4 }}
+            direction="column"
+          >
+            <PokemonList />
             <Pagination />
-          </Box>
+          </Flex>
         </Box>
       </HomepageLayout>
     </React.Fragment>
   );
 };
 
-const Pagination = () => {
+const Section1 = () => {
   return (
-    <Box>
-      <Flex justify="flex-end">
-        <Text fontSize="sm">Page 1-10 of 20</Text>
-      </Flex>
+    <HStack justify="space-between" as="section">
+      <Heading fontSize="2xl" color="brand.gray.100" fontWeight="semibold">
+        Choose Pokemon
+      </Heading>
 
-      <Center>
-        <HStack>
-          <IconButton
-            aria-label=""
-            bgColor="transparent"
-            icon={<Icon as={ChevronLeftIcon} stroke="brand.gray.100" />}
-          />
-          <Button aria-label="" color="brand.gray.800">
-            1
-          </Button>
-          <Button aria-label="" bgColor="brand.primary" color="brand.gray.800">
-            2
-          </Button>
-          <IconButton
-            aria-label=""
-            bgColor="transparent"
-            icon={<Icon as={ChevronRightIcon} stroke="brand.gray.100" />}
-          />
-        </HStack>
-      </Center>
-    </Box>
-  );
-};
-
-const Items = () => {
-  const listView = useStore((state) => state.listView);
-  return listView ? <TableView /> : <GridView />;
-};
-
-const GridView = () => {
-  return (
-    <SimpleGrid columns={{ base: 1, md: 2, lg: 4 }} gap={{ base: 4, lg: 8 }}>
-      {Array(8)
-        .fill(1)
-        .map((val, idx) => val + idx)
-        .map((v) => (
-          <Link key={v} passHref href={"/pokemons/" + v}>
-            <GridItem
-              as="a"
-              h="260px"
-              rounded="sm"
-              shadow="md"
-              position="relative"
-              overflow="hidden"
-            >
-              <NextImage
-                src={`/assets/samples/${v}.png`}
-                alt=""
-                layout="fill"
-                objectFit="cover"
-                objectPosition="center"
-              />
-            </GridItem>
-          </Link>
-        ))}
-    </SimpleGrid>
-  );
-};
-
-const TableView = () => {
-  return (
-    <Table bgColor="brand.gray.700" color="brand.gray.50">
-      <Thead fontSize="sm" fontWeight="bold">
-        <Tr>
-          <FitTd>#</FitTd>
-          <FitTd></FitTd>
-          <Td>Pokemon</Td>
-          <Td display={{ base: "none", md: "table-cell" }}>Type</Td>
-          <Td display={{ base: "none", lg: "table-cell" }}>Level</Td>
-        </Tr>
-      </Thead>
-
-      <Tbody>
-        {new Array(10).fill(null).map((_, idx) => {
-          return (
-            <Tr key={idx}>
-              <FitTd>1</FitTd>
-              <FitTd>
-                <Avatar w="32px" h="32px" />
-              </FitTd>
-              <Td>Pikachu</Td>
-              <Td display={{ base: "none", md: "table-cell" }}>Electric</Td>
-              <Td display={{ base: "none", lg: "table-cell" }}>Lvl 3</Td>
-            </Tr>
-          );
-        })}
-      </Tbody>
-    </Table>
-  );
-};
-
-const FitTd = ({ children }: React.PropsWithChildren<{}>) => {
-  return (
-    <Td w="1%" whiteSpace="nowrap">
-      {children}
-    </Td>
+      <Toolbar />
+    </HStack>
   );
 };
 
 const Toolbar = () => {
   const toggleListView = useStore((state) => state.toggleListView);
-  const handleToggle = (value?: boolean) => {
-    return () => toggleListView(value);
-  };
+  const handleToggle = (value?: boolean) => () => toggleListView(value);
 
   return (
     <Wrap spacing={8}>
@@ -203,16 +93,16 @@ const Toolbar = () => {
   );
 };
 
+interface ToolbarIconProps {
+  icon: (props: React.ComponentProps<"svg">) => JSX.Element;
+}
+
+const ToolbarIcon = ({ icon }: ToolbarIconProps) => {
+  return <Icon as={icon} fill="brand.gray.200" fontSize="xl" display="block" />;
+};
+
 const FilterTool = () => {
-  // prettier-ignore
-  const items = [
-    "Normal",
-    "Fire",
-    "Water",
-    "Grass",
-    "Flying",
-    "Fighting"
-  ];
+  const items = "Normal|Fire|Water|Grass|Flying|Fighting".split(/\|/);
 
   return (
     <Menu closeOnSelect={false}>
@@ -231,12 +121,139 @@ const FilterTool = () => {
   );
 };
 
-interface ToolbarIconProps {
-  icon: (props: React.ComponentProps<"svg">) => JSX.Element;
+const PokemonList = () => {
+  const listView = useStore((state) => state.listView);
+  return !!listView ? <ListView /> : <GridView />;
+};
+
+const GridView = () => {
+  return (
+    <SimpleGrid columns={{ base: 1, md: 2, lg: 4 }} gap={{ base: 4, lg: 8 }}>
+      {Array(8)
+        .fill(1)
+        .map((val, idx) => val + idx)
+        .map((v) => (
+          <GridViewItem key={v} v={v} />
+        ))}
+    </SimpleGrid>
+  );
+};
+
+const GridViewItem = ({ v }: any) => {
+  return (
+    <Link key={v} passHref href={"/pokemons/" + v}>
+      <GridItem
+        as="a"
+        h="260px"
+        rounded="sm"
+        shadow="md"
+        position="relative"
+        overflow="hidden"
+      >
+        <NextImage
+          src={`/assets/samples/${v}.png`}
+          alt=""
+          layout="fill"
+          objectFit="cover"
+          objectPosition="center"
+        />
+      </GridItem>
+    </Link>
+  );
+};
+
+const listViewHeadings = "#||Pokemon|Type|Level".split(/\|/);
+
+const ListView = () => {
+  return (
+    <GridTable
+      columns="49px 75px 1fr 1fr 1fr"
+      bgColor="brand.gray.700"
+      color="brand.gray.50"
+    >
+      <GridTableRow
+        px={4}
+        py={3}
+        borderColor="brand.gray.300"
+        borderBottom="1px"
+        fontSize="sm"
+        color="brand.gray.100"
+      >
+        {listViewHeadings.map((heading) => (
+          <GridTableHeading key={heading}>{heading}</GridTableHeading>
+        ))}
+      </GridTableRow>
+
+      {Array(10)
+        .fill("1|/assets/samples/2.png|Pikachu|Electric|Lvl 3".split(/\|/))
+        .map(([id, image, name, type, level], idx) => {
+          return (
+            <GridTableRow
+              key={idx}
+              py={3}
+              px={4}
+              borderColor="brand.gray.300"
+              borderBottom="1px"
+            >
+              <GridTableCell>{id}</GridTableCell>
+              <GridTableCell>
+                <Avatar
+                  src={image}
+                  w="32px"
+                  h="32px"
+                  showBorder
+                  borderColor="brand.gray.400"
+                />
+              </GridTableCell>
+              <GridTableCell>{name}</GridTableCell>
+              <GridTableCell>{type}</GridTableCell>
+              <GridTableCell>{level}</GridTableCell>
+            </GridTableRow>
+          );
+        })}
+    </GridTable>
+  );
+};
+
+const Pagination = () => {
+  return (
+    <Box>
+      <Flex justify="flex-end">
+        <Text fontSize="sm">Page 1-10 of 20</Text>
+      </Flex>
+
+      <Center>
+        <HStack>
+          <PageControl action="prev" />
+          <Button aria-label="" color="brand.gray.800" bgColor="brand.gray.100">
+            1
+          </Button>
+          <Button aria-label="" bgColor="brand.primary" color="brand.gray.800">
+            2
+          </Button>
+          <PageControl action="next" />
+        </HStack>
+      </Center>
+    </Box>
+  );
+};
+
+interface PageControlProps {
+  action: "next" | "prev";
+  onTrigger?: () => void;
 }
 
-const ToolbarIcon = ({ icon }: ToolbarIconProps) => {
-  return <Icon as={icon} fill="brand.gray.200" fontSize="xl" display="block" />;
+const PageControl = ({ action, onTrigger }: PageControlProps) => {
+  const icon = action === "next" ? ChevronRightIcon : ChevronLeftIcon;
+
+  return (
+    <IconButton
+      aria-label=""
+      icon={<Icon as={icon} stroke="brand.gray.100" />}
+      bgColor="transparent"
+      onClick={onTrigger}
+    />
+  );
 };
 
 export default Pokemons;
