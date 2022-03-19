d9c88af348650fe5d6aedb7f8e28315af3ed4d88
jp calvo <calvojp92@gmail.com>
2022-03-19 10:47:42 +0800

    added default border color

--- pokedex/src/components/widgets/grid-table/GridTableRow.tsx
+++ pokedex/src/components/widgets/grid-table/GridTableRow.tsx
@@ -1,7 +1,11 @@
 import { Grid, GridProps } from "@chakra-ui/react";
 
 const GridTableRow = ({ children, ...props }: GridProps) => {
-  return <Grid {...props}>{children}</Grid>;
+  return (
+    <Grid borderColor="brand.gray.50" {...props}>
+      {children}
+    </Grid>
+  );
 };
 
 export default GridTableRow;
