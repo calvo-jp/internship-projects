af32725b4acef1e40657b504570a844bea210d40
jp calvo <calvojp92@gmail.com>
2022-03-19 22:03:14 +0800

    focusing on auth

--- pokedex/src/config/apollo/client.ts
+++ pokedex/src/config/apollo/client.ts
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 const apolloClient = new ApolloClient({
-  uri: process.env.NEXT_PUBLIC_GRAPHQL_ENDPOINT_MAIN,
+  uri: process.env.NEXT_PUBLIC_GRAPHQL_ENDPOINT_AUTH,
   cache: new InMemoryCache(),
 });
 
