1ca0b3df357e9deaeef6cf7c1170fcf720578690
jp calvo <calvojp92@gmail.com>
2022-03-19 12:53:42 +0800

    refactor

--- pokedex/src/components/layouts/homepage/Header.tsx
+++ pokedex/src/components/layouts/homepage/Header.tsx
@@ -11,18 +11,11 @@ import {
   Text,
 } from "@chakra-ui/react";
 import ChevronDownIcon from "@heroicons/react/outline/ChevronDownIcon";
+import Link from "components/widgets/Link";
 import { signOut } from "next-auth/react";
-import Link from "next/link";
 import { useRouter } from "next/router";
 
 const Header = () => {
-  const { push } = useRouter();
-
-  const logout = async () => {
-    await signOut({ redirect: false });
-    await push("/");
-  };
-
   return (
     <Flex
       as="header"
@@ -36,30 +29,40 @@ const Header = () => {
       pos="sticky"
       top={0}
     >
-      <Link passHref href="/pokemons">
-        <a>
-          <Heading as="h1" fontSize="4xl" color="brand.amber.400">
-            Pokedex
-          </Heading>
-        </a>
+      <Link href="/pokemons" _hover={{ textDecor: "none" }}>
+        <Heading as="h1" fontSize="4xl" color="brand.amber.400">
+          Pokedex
+        </Heading>
       </Link>
 
       <HStack spacing={5}>
         <Text>Welcome, JP</Text>
         <Avatar w="57px" h="57px" showBorder borderColor="brand.primary" />
-
-        <Menu>
-          <MenuButton>
-            <Icon as={ChevronDownIcon} fontSize="xl" stroke="brand.gray.100" />
-          </MenuButton>
-          <MenuList mt={3} bgColor="white">
-            <MenuItem color="brand.red.500" onClick={logout}>
-              Logout
-            </MenuItem>
-          </MenuList>
-        </Menu>
+        <Dropdown />
       </HStack>
     </Flex>
+  );
+};
+
+const Dropdown = () => {
+  const { push } = useRouter();
+
+  const logout = async () => {
+    await signOut({ redirect: false });
+    await push("/");
+  };
+
+  return (
+    <Menu>
+      <MenuButton>
+        <Icon as={ChevronDownIcon} fontSize="xl" stroke="brand.gray.100" />
+      </MenuButton>
+      <MenuList mt={3} bgColor="white">
+        <MenuItem color="brand.red.500" onClick={logout}>
+          Logout
+        </MenuItem>
+      </MenuList>
+    </Menu>
   );
 };
 
