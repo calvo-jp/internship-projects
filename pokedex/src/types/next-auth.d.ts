9d741c3c0eb9e5a8e1e6d9bca66ae4fd3b7f374b
jp calvo <calvojp92@gmail.com>
2022-03-19 22:44:20 +0800

    added user blueprint in useSession or getSession

--- pokedex/src/types/next-auth.d.ts
+++ pokedex/src/types/next-auth.d.ts
@@ -3,7 +3,15 @@ import { JWT } from "next-auth/jwt";
 
 // ref: https://next-auth.js.org/getting-started/typescript
 declare module "next-auth" {
-  interface Session {}
+  interface Session {
+    user: {
+      id: string;
+      name: string;
+      email: string;
+      image?: string;
+    };
+  }
+
   interface User {}
   interface Account {}
   interface Profile {}
