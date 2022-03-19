7220aeec12eca6360c075e1fb1d559418e467f15
jp calvo <calvojp92@gmail.com>
2022-03-19 22:46:53 +0800

    append token if found

--- pokedex/src/config/apollo/client.ts
+++ pokedex/src/config/apollo/client.ts
@@ -1,8 +1,28 @@
-import { ApolloClient, InMemoryCache } from "@apollo/client";
+import { ApolloClient, from, HttpLink, InMemoryCache } from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
+import { getSession } from "next-auth/react";
+
+const authLink = setContext(async (_, originalContext) => {
+  const session = await getSession();
+
+  if (!session) return originalContext;
+
+  return {
+    ...originalContext,
+    headers: {
+      ...originalContext.headers,
+      Authorization: "Bearer " + session.accessToken,
+    },
+  };
+});
+
+const link = new HttpLink({
+  uri: process.env.NEXT_PUBLIC_GRAPHQL_ENDPOINT_AUTH,
+});
 
 const apolloClient = new ApolloClient({
-  uri: process.env.NEXT_PUBLIC_GRAPHQL_ENDPOINT_AUTH,
   cache: new InMemoryCache(),
+  link: from([authLink, link]),
 });
 
 export default apolloClient;
