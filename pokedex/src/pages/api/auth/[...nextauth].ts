f15f42517f1cb37992fd47cdcf492a8bbb0ecae1
jp calvo <calvojp92@gmail.com>
2022-03-20 19:20:39 +0800

    workarround updates

--- pokedex/src/pages/api/auth/[...nextauth].ts
+++ pokedex/src/pages/api/auth/[...nextauth].ts
@@ -30,7 +30,7 @@ export default NextAuth({
       async authorize(credentials) {
         if (!credentials) return null;
 
-        const data = await loginSchema.validate(credentials);
+        const data = await schema.validate(credentials);
         const accessToken = await authenticate(data);
 
         return !!accessToken ? { accessToken } : null;
@@ -38,13 +38,46 @@ export default NextAuth({
     }),
   ],
   callbacks: {
-    async jwt({ user, token }) {
-      if (user) token.accessToken = user.accessToken;
+    async jwt({ user, token, account, profile }) {
+      if (account) {
+        // store provider profile to user object
+        if (account.type === "oauth") {
+          token.accessToken = account.access_token || account.oauth_token;
+
+          // facebook: id, name, email, picture.data.url
+          // twitter: id, name, email, profile_image_url
+          // linkedin: id, localizedFirstName, localizedLastName,
+
+          token.user = {
+            name: token.name,
+            email: token.email,
+            image: token.picture,
+
+            // the "__auth_method__" prop will serve as a type-guard
+            // and will mostly be used in the client
+            // see next-auth.d.ts
+
+            __auth_method__: "oauth",
+          };
+        }
+
+        // for credentials,
+        // profile details will be fetched in the client side
+        // to obtain the most updated info
+        if (account.type === "credentials" && user) {
+          token.accessToken = user.accessToken;
+          token.user = {
+            __auth_method__: "credential",
+          };
+        }
+      }
 
       return token;
     },
     async session({ token, session }) {
-      return { ...session, ...token };
+      session.user = token.user as any;
+      session.accessToken = token.accessToken as string;
+      return session;
     },
   },
   pages: {
@@ -52,12 +85,12 @@ export default NextAuth({
   },
 });
 
-const loginSchema = yup.object().shape({
+const schema = yup.object().shape({
   email: yup.string().email().required(),
   password: yup.string().min(5).max(100).required(),
 });
 
-const authenticate = async (credentials: yup.InferType<typeof loginSchema>) => {
+const authenticate = async (credentials: yup.InferType<typeof schema>) => {
   const result = await apolloClient.mutate<Authenticate, AuthenticateVariables>(
     { mutation: AUTHENTICATE, variables: credentials }
   );
