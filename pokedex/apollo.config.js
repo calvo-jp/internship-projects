09b730b55be69483bc4164d4f9953844885ec17c
jp calvo <calvojp92@gmail.com>
2022-03-21 11:23:37 +0800

    using dotenv

--- pokedex/apollo.config.js
+++ pokedex/apollo.config.js
@@ -1,10 +1,9 @@
 module.exports = {
   client: {
-    includes: ["./src/graphql/**/*.ts"],
     service: {
       skipSSLValidation: true,
       name: "PokeAPI",
-      url: "https://frontend-engineer-onboarding-api-thxaa.ondigitalocean.app/graphql",
+      url: process.env.NEXT_PUBLIC_GRAPHQL_ENDPOINT_MAIN,
     },
   },
 };
