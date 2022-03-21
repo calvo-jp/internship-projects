42fbec4b81d7957559e5fce02e03c1f55447bf12
jp calvo <calvojp92@gmail.com>
2022-03-21 21:03:06 +0800

    updates

--- pokedex/src/pages/pokemons/index.tsx
+++ pokedex/src/pages/pokemons/index.tsx
@@ -1,8 +1,6 @@
 import {
   Avatar,
   Box,
-  Button,
-  Center,
   Checkbox,
   Flex,
   Heading,
@@ -15,6 +13,7 @@ import {
   MenuItem,
   MenuList,
   SimpleGrid,
+  Spinner,
   Text,
   Wrap,
   WrapItem,
@@ -22,19 +21,45 @@ import {
 import { ChevronLeftIcon, ChevronRightIcon } from "@heroicons/react/outline";
 import { FilterIcon, ViewGridIcon, ViewListIcon } from "@heroicons/react/solid";
 import HomepageLayout from "components/layouts/homepage";
+import Button from "components/widgets/Button";
 import GridTable from "components/widgets/grid-table";
 import GridTableCell from "components/widgets/grid-table/GridTableCell";
 import GridTableHeading from "components/widgets/grid-table/GridTableHeading";
 import GridTableRow from "components/widgets/grid-table/GridTableRow";
+import ImageWithFallback from "components/widgets/ImageWithFallback";
+import apolloClient from "config/apollo/client";
+import { GET_POKEMONS } from "graphql/pokeapi/queries";
+import usePagination from "hooks/usePagination";
 import useStore from "hooks/useStore";
+import { GetStaticProps } from "next";
 import Head from "next/head";
-import NextImage from "next/image";
 import Link from "next/link";
 import { useRouter } from "next/router";
 import * as React from "react";
-import valx from "utils/valx";
+import { GetPokemons, GetPokemonsVariables } from "__generated__/GetPokemons";
 
-const Pokemons = () => {
+interface Props {
+  pokemons: GetPokemons["pokemons"];
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const { data } = await apolloClient.query<GetPokemons, GetPokemonsVariables>({
+    query: GET_POKEMONS,
+    variables: {
+      limit: 150,
+    },
+  });
+
+  return {
+    props: {
+      pokemons: data.pokemons,
+    },
+  };
+};
+
+const Pokemons = ({ pokemons }: Props) => {
+  const pagination = usePagination(pokemons);
+
   return (
     <React.Fragment>
       <Head>
@@ -58,11 +83,11 @@ const Pokemons = () => {
           <Flex
             as="section"
             mt={{ base: 6, lg: 12 }}
-            gap={{ base: 2, lg: 4 }}
+            gap={{ base: 4, lg: 8 }}
             direction="column"
           >
-            <PokemonList />
-            <Pagination />
+            <PokemonList pokemons={pagination.rows} />
+            <Pagination {...pagination} />
           </Flex>
         </Box>
       </HomepageLayout>
@@ -125,50 +150,67 @@ const FilterTool = () => {
   );
 };
 
-const PokemonList = () => {
+const PokemonList = (props: Props) => {
+  // this causes hydration issue if used directly
   const listView = useStore((state) => state.listView);
-  return !!listView ? <ListView /> : <GridView />;
+  const [isListView, setListView] = React.useState<boolean>();
+
+  React.useEffect(() => {
+    setListView(listView);
+    return () => setListView(false);
+  }, [listView]);
+
+  return !!isListView ? <ListView {...props} /> : <GridView {...props} />;
 };
 
-const GridView = () => {
+type GridViewProps = Props;
+
+const GridView = ({ pokemons }: GridViewProps) => {
   return (
-    <SimpleGrid columns={{ base: 1, md: 2, lg: 4 }} gap={{ base: 4, lg: 8 }}>
-      {Array(8)
-        .fill(1)
-        .map((val, idx) => val + idx)
-        .map((v) => (
-          <GridViewItem key={v} v={v} />
-        ))}
+    <SimpleGrid columns={{ md: 2, lg: 4 }} gap={{ base: 4, lg: 8 }}>
+      {pokemons.map((pokemon) => (
+        <GridViewItem key={pokemon.id} data={pokemon} />
+      ))}
     </SimpleGrid>
   );
 };
 
-const GridViewItem = ({ v }: any) => {
+interface GridViewItemProps {
+  data: Props["pokemons"][number];
+}
+
+const GridViewItem = ({ data }: GridViewItemProps) => {
   return (
-    <Link key={v} passHref href={"/pokemons/" + v}>
+    <Link passHref href={"/pokemons/" + data.id}>
       <LinkBox
         as="a"
         h="260px"
+        bgColor="brand.gray.800"
         rounded="sm"
         shadow="md"
         position="relative"
         overflow="hidden"
+        display="flex"
+        alignItems="center"
+        justifyContent="center"
       >
-        <NextImage
-          src={`/assets/samples/${v}.png`}
+        <ImageWithFallback
+          maxW="80%"
+          maxH="80%"
+          src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${data.id}.svg`}
           alt=""
-          layout="fill"
-          objectFit="cover"
-          objectPosition="center"
+          loader={<Spinner size="xl" />}
         />
       </LinkBox>
     </Link>
   );
 };
 
-const ListView = () => {
+type ListViewProps = Props;
+
+const ListView = ({ pokemons }: ListViewProps) => {
   const router = useRouter();
-  const handleClick = (id: string) => () => router.push(`/pokemons/${id}`);
+  const handleClick = (id: number) => () => router.push("/pokemons/" + id);
 
   return (
     <GridTable columns="49px 75px 1fr 1fr 1fr" bgColor="brand.gray.700">
@@ -178,95 +220,69 @@ const ListView = () => {
         borderColor="brand.gray.300"
         borderBottom="1px"
       >
-        {"#||Pokemon|Type|Level".split(/\|/).map((heading) => (
+        {"#----Pokemon--Type--Level".split(/--/).map((heading) => (
           <GridTableHeading key={heading}>{heading}</GridTableHeading>
         ))}
       </GridTableRow>
 
-      {Array(10)
-        .fill("1|/assets/samples/2.png|Pikachu|Electric|Lvl 3".split(/\|/))
-        .map(([id, image, name, type, level], idx) => (
-          <GridTableRow
-            key={idx}
-            py={3}
-            px={4}
-            borderColor="brand.gray.300"
-            borderBottom="1px"
-            cursor="pointer"
-            _hover={{ bgColor: "brand.gray.600" }}
-            onClick={handleClick(id)}
-          >
-            <GridTableCell>{id}</GridTableCell>
-            <GridTableCell>
-              <Avatar
-                src={image}
-                w="32px"
-                h="32px"
-                showBorder
-                borderColor="brand.gray.400"
-              />
-            </GridTableCell>
-            <GridTableCell>{name}</GridTableCell>
-            <GridTableCell>{type}</GridTableCell>
-            <GridTableCell>{level}</GridTableCell>
-          </GridTableRow>
-        ))}
+      {pokemons.map(({ id, name, types }) => (
+        <GridTableRow
+          key={id}
+          py={3}
+          px={4}
+          borderColor="brand.gray.300"
+          borderBottom="1px"
+          cursor="pointer"
+          _hover={{ bgColor: "brand.gray.600" }}
+          onClick={handleClick(id)}
+        >
+          <GridTableCell>{id}</GridTableCell>
+          <GridTableCell>
+            <Avatar
+              w="32px"
+              h="32px"
+              src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${id}.svg`}
+              bgColor="transparent"
+            />
+          </GridTableCell>
+          <GridTableCell>{name}</GridTableCell>
+          <GridTableCell>
+            {types.map((type) => type.type?.name || "").join()}
+          </GridTableCell>
+          <GridTableCell>1</GridTableCell>
+        </GridTableRow>
+      ))}
     </GridTable>
   );
 };
 
-const Pagination = () => {
+type PaginationProps = ReturnType<typeof usePagination>;
+
+const Pagination = ({ prev, next }: PaginationProps) => {
   return (
-    <Box>
-      <Flex justify="flex-end">
-        <Text fontSize="sm">Page 1-10 of 20</Text>
-      </Flex>
+    <HStack spacing={8} justify="center">
+      <HStack spacing={4}>
+        <IconButton
+          aria-label=""
+          icon={<Icon as={ChevronLeftIcon} />}
+          bgColor="brand.primary"
+          color="brand.gray.800"
+          rounded="full"
+          shadow="md"
+          onClick={prev}
+        />
 
-      <Center>
-        <HStack>
-          <PageControl action="prev" />
-          {Array(2)
-            .fill(1)
-            .map((n, idx) => n + idx)
-            .map((value) => (
-              <Button
-                aria-label=""
-                key={value}
-                bgColor={valx({
-                  "brand.primary": value === 1,
-                  "brand.gray.800": value > 1,
-                })}
-                color={valx({
-                  "brand.gray.800": value === 1,
-                  "brand.gray.100": value > 1,
-                })}
-              >
-                {value}
-              </Button>
-            ))}
-
-          <PageControl action="next" />
-        </HStack>
-      </Center>
-    </Box>
-  );
-};
-
-interface PageControlProps {
-  action: "next" | "prev";
-  onTrigger?: () => void;
-}
-
-const PageControl = ({ action, onTrigger }: PageControlProps) => {
-  const icon = action === "next" ? ChevronRightIcon : ChevronLeftIcon;
-
-  return (
-    <IconButton
-      aria-label=""
-      icon={<Icon as={icon} stroke="brand.gray.100" />}
-      bgColor="transparent"
-      onClick={onTrigger}
-    />
+        <IconButton
+          aria-label=""
+          icon={<Icon as={ChevronRightIcon} />}
+          bgColor="brand.primary"
+          color="brand.gray.800"
+          rounded="full"
+          shadow="md"
+          onClick={next}
+        />
+      </HStack>
+    </HStack>
   );
 };
 
