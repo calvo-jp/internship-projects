672ce121817c79b5075ac305b72a7dc82e1828e8
jp calvo <calvojp92@gmail.com>
2022-03-21 18:23:15 +0800

    updated quries

--- pokedex/src/graphql/pokeapi/queries.ts
+++ pokedex/src/graphql/pokeapi/queries.ts
@@ -14,11 +14,85 @@ export const CORE_POKEMON_DETAILS = gql`
 `;
 
 export const GET_POKEMONS = gql`
-  query GetPokemons {
-    pokemon_v2_pokemon {
+  query GetPokemons($limit: Int = 100, $offset: Int = 0) {
+    pokemons: pokemon_v2_pokemon(limit: $limit, offset: $offset) {
       ...CorePokemonDetails
     }
   }
 
   ${CORE_POKEMON_DETAILS}
+`;
+
+export const GET_POKEMON = gql`
+  query GetPokemon($id: Int!) {
+    pokemon: pokemon_v2_pokemon_by_pk(id: $id) {
+      weight
+      height
+
+      species: pokemon_v2_pokemonspecy {
+        id
+        name
+        pokemon_v2_pokemonspeciesflavortexts {
+          id
+          flavor_text
+        }
+        eggGroups: pokemon_v2_pokemonegggroups {
+          id
+          eggGroup: pokemon_v2_egggroup {
+            name
+          }
+        }
+        color: pokemon_v2_pokemoncolor {
+          id
+          name
+        }
+        descriptions: pokemon_v2_pokemonspeciesdescriptions {
+          id
+          description
+        }
+        pokemon_v2_generation {
+          name
+
+        }
+      }
+
+      ...CorePokemonDetails
+    }
+
+    ${CORE_POKEMON_DETAILS}
+  }
+`;
+
+export const GET_POKEMON_STATS = gql`
+  query GetPokemonStats($id: Int!) {
+    pokemon: pokemon_v2_pokemon_by_pk(id: $id) {
+      stats: pokemon_v2_pokemonstats {
+        id
+        value: base_stat
+        stat: pokemon_v2_stat {
+          name
+        }
+      }
+    }
+  }
+`;
+
+export const GET_POKEMON_MOVES = gql`
+  query GetPokemonMoves($id: Int!) {
+    pokemon: pokemon_v2_pokemon_by_pk(id: $id) {
+      main: pokemon_v2_pokemonabilities {
+        id
+        move: pokemon_v2_ability {
+          name
+        }
+      }
+
+      quick: pokemon_v2_pokemonmoves(distinct_on: id, limit: 9) {
+        id
+        move: pokemon_v2_move {
+          name
+        }
+      }
+    }
+  }
 `;
