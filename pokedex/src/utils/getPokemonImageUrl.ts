eee7d01b034cdec5de3f97ce563a0cf0148ea690
jp calvo <calvojp92@gmail.com>
2022-03-22 17:36:28 +0800

    refactor

--- pokedex/src/utils/getPokemonImageUrl.ts
+++ pokedex/src/utils/getPokemonImageUrl.ts
@@ -1,5 +1,11 @@
-const getPokemonImageUrl = (id: number | string) => {
-  return `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${id}.svg`;
+type ImageType = "SVG" | "PNG";
+
+const getPokemonImageUrl = (id: number | string, extension?: ImageType) => {
+  const ext = extension === "SVG" ? ".svg" : ".png";
+  const cdn =
+    "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/";
+
+  return cdn + id + ext;
 };
 
 export default getPokemonImageUrl;
