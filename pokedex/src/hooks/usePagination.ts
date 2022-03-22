d7c7482f5835d3d0e42a3579fbf066c1414633c8
jp calvo <calvojp92@gmail.com>
2022-03-22 17:24:43 +0800

    usePagination hook updates

--- pokedex/src/hooks/usePagination.ts
+++ pokedex/src/hooks/usePagination.ts
@@ -1,57 +1,132 @@
+import { useRouter } from "next/router";
 import * as React from "react";
 import arrayChunk from "utils/arrayChunk";
 
 interface Config {
-  rowsPerPage?: number;
-  currentPage?: number;
+  onPageChange?: (newValue: number) => void;
+  onPageSizeChange?: (newValue: number) => void;
 }
 
+/**
+ *
+ * @description
+ * router based pagination
+ *
+ * @example
+ * const router = useRouter();
+ *
+ * const {
+ *   next,
+ *   prev,
+ *   rows,
+ *   page,
+ *   pageSize,
+ *   totalRows,
+ *   totalPages,
+ *   changePageSize,
+ * } = usePagination(avengers, {
+ *   onPageChange(page) {
+ *     router.push(router.basePath, {
+ *       query: {
+ *         page,
+ *         pageSize,
+ *       },
+ *     });
+ *   },
+ *   onPageSizeChange(pageSize) {
+ *     router.push(router.basePath, {
+ *       query: {
+ *         page,
+ *         pageSize,
+ *       },
+ *     });
+ *   },
+ * });
+ *
+ */
 const usePagination = <T extends Array<any>>(data: T, config: Config = {}) => {
-  const [loading, setLoading] = React.useState(true);
-  const [currentPage, setCurrentPage] = React.useState(1);
-  const [rowsPerPage, setRowsPerPage] = React.useState(20);
+  const router = useRouter();
+
+  const [page, setPage] = React.useState(1);
+  const [pageSize, setPageSize] = React.useState(5);
   const [chunks, setChunks] = React.useState<T[]>([]);
   const [hasNext, setHasNext] = React.useState(false);
   const [hasPrev, setHasPrev] = React.useState(false);
 
+  const increment = (by = 1) => {
+    setPage((old) => {
+      const current = old + by;
+      config.onPageChange && config.onPageChange(current);
+      return current;
+    });
+  };
+
+  const decrement = (by = 1) => {
+    setPage((old) => {
+      const current = old - by;
+      config.onPageChange && config.onPageChange(current);
+      return current;
+    });
+  };
+
+  const changePageSize = (newValue: number) => {
+    setPageSize(() => {
+      config.onPageSizeChange && config.onPageSizeChange(newValue);
+      return newValue;
+    });
+  };
+
   const next = () => {
-    if (hasNext) setCurrentPage((n) => n + 1);
+    if (!hasNext) return;
+    increment();
   };
 
   const prev = () => {
-    if (hasPrev) setCurrentPage((n) => n - 1);
+    if (!hasPrev) return;
+    decrement();
   };
 
   React.useEffect(() => {
-    if (config.currentPage) setCurrentPage(config.currentPage);
-    if (config.rowsPerPage) setRowsPerPage(config.rowsPerPage);
-    setChunks(arrayChunk(data, rowsPerPage));
-    setHasNext(currentPage < chunks.length);
-    setHasPrev(currentPage > 1);
-    setLoading(false);
-  }, [
-    chunks.length,
-    config.currentPage,
-    config.rowsPerPage,
-    currentPage,
-    data,
-    rowsPerPage,
-  ]);
+    setHasNext(page < chunks.length);
+    setHasPrev(page > 1);
+    setChunks(arrayChunk(data, pageSize));
+
+    const p = queryToIntOrUndefined(router.query.page);
+    const s = queryToIntOrUndefined(router.query.pageSize);
+
+    if (p && p !== page) setPage(p);
+    if (s && s !== pageSize) setPageSize(s);
+  }, [chunks.length, data, page, pageSize, router.query]);
 
   return {
-    loading,
-    rows: chunks.at(currentPage - 1) || [],
-    totalRows: data.length,
-    rowsPerPage,
-    currentPage,
+    /** current page index */
+    page,
+    /** aka. rows-per-page */
+    pageSize,
+    /** number of pages based on pageSize */
     totalPages: chunks.length,
-    prev,
-    next,
-    hasPrev,
+    /** total number of data */
+    totalRows: data.length,
+    /** portion of data which is based on current page and pageSize  */
+    rows: chunks.at(page - 1) || [],
+    /** checks if a next page exists */
     hasNext,
-    prevPage: hasPrev ? currentPage - 1 : null,
-    nextPage: hasNext ? currentPage + 1 : null,
+    /** checks if a previous page exists */
+    hasPrev,
+    /** navigate to next page */
+    next,
+    /** navigate to previous page */
+    prev,
+    /** changes the size or rows per page */
+    changePageSize,
   };
 };
+
+const queryToIntOrUndefined = (subject: string[] | string | undefined) => {
+  const scalar = [subject].flat(1).at(0);
+  if (scalar && isNumeric(scalar)) return parseInt(scalar);
+};
+
+const isNumeric = (subject: string) => /\d+/.test(subject);
 
 export default usePagination;
