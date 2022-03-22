173975eb8977f8b228c58ec7b35fbe3b4ba0dc81
jp calvo <calvojp92@gmail.com>
2022-03-22 12:28:19 +0800

    updated store

--- pokedex/src/hooks/useStore.ts
+++ pokedex/src/hooks/useStore.ts
@@ -4,35 +4,32 @@ import { devtools, persist } from "zustand/middleware";
 interface Store {
   listView?: boolean;
   toggleListView: (value?: boolean) => void;
-  /**
-   *
-   * previously viewed pokemons
-   *
-   */
-  history: [];
-  /**
-   *
-   * add new item to previously viewed pokemons
-   *
-   */
-  saveHistory: () => void;
-  clearHistory: () => void;
+  viewedPokemonIds: number[];
+  saveAsViewedPokemon: (id: number) => void;
 }
 
 const useStore = createStore<Store>(
   persist(
-    devtools((set) => ({
+    devtools((set, get) => ({
       toggleListView: (value) => {
         return set((state) => ({
           listView: typeof value === "boolean" ? value : !state.listView,
         }));
       },
-      history: [],
-      saveHistory: () => {},
-      clearHistory: () => {
-        return set(() => ({
-          history: [],
-        }));
+      viewedPokemonIds: [],
+      saveAsViewedPokemon: (id) => {
+        return set(() => {
+          // copy
+          const current = [...get().viewedPokemonIds];
+          // remove from viewed ids if it exists
+          const filtered = current.includes(id)
+            ? current.filter((currentId) => id !== currentId)
+            : current;
+
+          return {
+            viewedPokemonIds: [id /* should be the most recent*/, ...filtered],
+          };
+        });
       },
     })),
     { name: "POKEDEX" }
