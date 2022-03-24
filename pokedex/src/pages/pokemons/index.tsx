64b4ff01545aadcdd83caaae845f9a822228f4f3
jp calvo <calvojp92@gmail.com>
2022-03-24 18:26:56 +0800

    updates

--- pokedex/src/pages/pokemons/index.tsx
+++ pokedex/src/pages/pokemons/index.tsx
@@ -1,40 +1,17 @@
-import {
-  Box,
-  Checkbox,
-  Flex,
-  Heading,
-  HStack,
-  Icon,
-  IconButton,
-  Menu,
-  MenuButton,
-  MenuItem,
-  MenuList,
-  SimpleGrid,
-  Spinner,
-  Text,
-  Wrap,
-  WrapItem,
-} from "@chakra-ui/react";
-import { ChevronLeftIcon, ChevronRightIcon } from "@heroicons/react/outline";
-import { FilterIcon, ViewGridIcon, ViewListIcon } from "@heroicons/react/solid";
+import { Box, Flex, Heading, HStack } from "@chakra-ui/react";
 import HomepageLayout from "components/layouts/homepage";
-import GridTable from "components/widgets/gridTable";
-import GridTableCell from "components/widgets/gridTable/GridTableCell";
-import GridTableHeading from "components/widgets/gridTable/GridTableHeading";
-import GridTableRow from "components/widgets/gridTable/GridTableRow";
-import Thumbnail from "components/widgets/Thumbnail";
+import GridView from "components/pages/pokemon-list/GridView";
+import ListView from "components/pages/pokemon-list/ListView";
+import Pagination from "components/pages/pokemon-list/Pagination";
+import Toolbar from "components/pages/pokemon-list/Toolbar";
 import apolloClient from "config/apollo/client";
 import { GET_POKEMONS, GET_POKEMONS_BY_TYPES } from "graphql/pokeapi/queries";
-import usePokemonTypes from "hooks/usePokemonTypes";
 import useStore from "hooks/useStore";
 import { GetServerSideProps } from "next";
 import Head from "next/head";
-import Link from "next/link";
 import { useRouter } from "next/router";
 import * as React from "react";
 import coalesce from "utils/coalesce";
-import getPokemonImageUrl from "utils/getPokemonImageUrl";
 import routerQueryValueToIntOrUndefined from "utils/routerQueryToIntOrUndefined";
 import { GetPokemons, GetPokemonsVariables } from "__generated__/GetPokemons";
 import {
@@ -189,232 +166,14 @@ const Pokemons = ({ rows, page, pageSize, hasNext, search }: Props) => {
             gap={{ base: 4, lg: 8 }}
             direction="column"
           >
-            {view === "list" && <ListView rows={rows} />}
-            {view === "grid" && <GridView rows={rows} />}
+            {view === "list" && <ListView data={rows} />}
+            {view === "grid" && <GridView data={rows} />}
 
-            <PageControls onNext={next} onPrev={prev} />
+            <Pagination onNext={next} onPrev={prev} />
           </Flex>
         </Box>
       </HomepageLayout>
     </React.Fragment>
-  );
-};
-
-interface ToolbarProps {
-  /** aka. pokemon types */
-  filters?: string[];
-  /** aka. onChange */
-  onFilter: (values: string[]) => void;
-}
-
-const Toolbar = ({ filters, onFilter }: ToolbarProps) => {
-  const toggleListView = useStore((state) => state.toggleListView);
-  const handleToggle = (value?: boolean) => () => toggleListView(value);
-
-  return (
-    <Wrap spacing={8}>
-      <WrapItem>
-        <FilterTool value={filters} onChange={onFilter} />
-      </WrapItem>
-      <WrapItem>
-        <button onClick={handleToggle(true)}>
-          <ToolbarIcon icon={ViewListIcon} />
-        </button>
-      </WrapItem>
-      <WrapItem>
-        <button onClick={handleToggle(false)}>
-          <ToolbarIcon icon={ViewGridIcon} />
-        </button>
-      </WrapItem>
-    </Wrap>
-  );
-};
-
-interface FilterToolProps {
-  value?: string[];
-  onChange: (filters: string[]) => void;
-}
-
-/** Controlled component */
-const FilterTool = ({ value = [], onChange }: FilterToolProps) => {
-  const categories = usePokemonTypes();
-
-  const handleChange = (newValue: string) => {
-    return (e: React.ChangeEvent<HTMLInputElement>) => {
-      // ticked
-      if (e.target.checked && !value.includes(newValue))
-        onChange([...value, newValue]);
-      // unticked
-      if (!e.target.checked && value.includes(newValue))
-        onChange(value.filter((item) => item !== newValue));
-    };
-  };
-
-  return (
-    <Menu closeOnSelect={false}>
-      <MenuButton>
-        <ToolbarIcon icon={FilterIcon} />
-      </MenuButton>
-      <MenuList
-        color="brand.gray.700"
-        bgColor="white"
-        maxH="60vh"
-        overflowY="auto"
-      >
-        {categories.map((item) => {
-          console.log(value.includes(item));
-          return (
-            <MenuItem key={item} display="flex" justifyContent="space-between">
-              <Text>{item}</Text>
-              <Checkbox
-                colorScheme="colorSchemeHacks.yellow"
-                borderColor="brand.gray.400"
-                iconColor="brand.primaryDark"
-                onChange={handleChange(item)}
-                isChecked={value.includes(item)}
-              />
-            </MenuItem>
-          );
-        })}
-      </MenuList>
-    </Menu>
-  );
-};
-
-interface ToolbarIconProps {
-  icon: (props: React.ComponentProps<"svg">) => JSX.Element;
-}
-
-const ToolbarIcon = ({ icon }: ToolbarIconProps) => {
-  return <Icon as={icon} fill="brand.gray.200" fontSize="xl" display="block" />;
-};
-
-interface GridViewProps {
-  rows: Pokemon[];
-}
-
-const GridView = ({ rows }: GridViewProps) => {
-  return (
-    <SimpleGrid columns={{ md: 2, lg: 4 }} gap={{ base: 4, lg: 8 }}>
-      {rows.map((pokemon) => (
-        <GridViewItem key={pokemon.id} data={pokemon} />
-      ))}
-    </SimpleGrid>
-  );
-};
-
-interface GridViewItemProps {
-  data: Pokemon;
-}
-
-const GridViewItem = ({ data }: GridViewItemProps) => {
-  return (
-    <Link href={"/pokemons/" + data.id} passHref>
-      <Thumbnail
-        as="a"
-        h="260px"
-        src={getPokemonImageUrl(data.id)}
-        loader={<Spinner size="xl" />}
-      />
-    </Link>
-  );
-};
-
-type ListViewProps = GridViewProps;
-
-const ListView = ({ rows }: ListViewProps) => {
-  const router = useRouter();
-  const handleClick = (id: number) => () => router.push("/pokemons/" + id);
-
-  return (
-    <GridTable columns="49px 75px 1fr 1fr 1fr" bgColor="brand.gray.700">
-      <GridTableRow
-        px={4}
-        py={3}
-        borderColor="brand.gray.300"
-        borderBottom="1px"
-      >
-        {"#||Pokemon|Type|Level".split(/\|/).map((heading) => (
-          <GridTableHeading key={heading}>{heading}</GridTableHeading>
-        ))}
-      </GridTableRow>
-
-      {rows.map(({ id, name, types }) => (
-        <GridTableRow
-          key={id}
-          py={3}
-          px={4}
-          borderColor="brand.gray.300"
-          borderBottom="1px"
-          cursor="pointer"
-          _hover={{ bgColor: "brand.gray.600" }}
-          _focus={{ bgColor: "brand.gray.600", outline: "none" }}
-          onClick={handleClick(id)}
-          tabIndex={1}
-        >
-          <GridTableCell>{id}</GridTableCell>
-          <GridTableCell>
-            <Thumbnail
-              w="32px"
-              h="32px"
-              src={getPokemonImageUrl(id)}
-              bgColor="transparent"
-              shadow="none"
-              loader={<Spinner size="md" />}
-            />
-          </GridTableCell>
-          <GridTableCell>{name}</GridTableCell>
-          <GridTableCell>
-            {types.map((type) => type.type?.name || "").join(", ")}
-          </GridTableCell>
-          <GridTableCell>Lvl 1</GridTableCell>
-        </GridTableRow>
-      ))}
-    </GridTable>
-  );
-};
-
-interface PaginationProps {
-  onNext: () => void;
-  onPrev: () => void;
-}
-
-const PageControls = ({ onNext, onPrev }: PaginationProps) => {
-  return (
-    <HStack spacing={8} justify="center">
-      <HStack spacing={4}>
-        <Control control="previous" onClick={onPrev} />
-        <Control control="next" onClick={onNext} />
-      </HStack>
-    </HStack>
-  );
-};
-
-interface ControlProps extends React.ComponentProps<"button"> {
-  control: "next" | "previous";
-}
-
-const Control = ({ control, ...props }: ControlProps) => {
-  return (
-    <IconButton
-      aria-label={control}
-      icon={
-        <Icon as={control === "next" ? ChevronRightIcon : ChevronLeftIcon} />
-      }
-      bgColor="brand.primary"
-      color="brand.gray.800"
-      rounded="full"
-      shadow="md"
-      _hover={{
-        bgColor: "brand.primary",
-        color: "brand.gray.800",
-      }}
-      _active={{
-        bgColor: "brand.primary",
-        color: "brand.gray.800",
-      }}
-      {...props}
-    />
   );
 };
 
