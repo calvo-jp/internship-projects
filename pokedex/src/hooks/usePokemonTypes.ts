6c87fe90d1ce3794fa70ac5082d6083c710e231d
jp calvo <calvojp92@gmail.com>
2022-03-25 02:47:33 +0800

    returning error and loading states

--- pokedex/src/hooks/usePokemonTypes.ts
+++ pokedex/src/hooks/usePokemonTypes.ts
@@ -3,12 +3,15 @@ import { GET_POKEMON_TYPES } from "graphql/pokeapi/queries";
 import { GetPokemonTypes } from "__generated__/GetPokemonTypes";
 
 const usePokemonTypes = () => {
-  const { loading, data } = useQuery<GetPokemonTypes>(GET_POKEMON_TYPES);
-  return !loading && data
-    ? data.pokemon_v2_type
-        .map(({ name }) => name)
-        .filter((value) => value !== "unknown")
-    : [];
+  const { loading, data, error } = useQuery<GetPokemonTypes>(GET_POKEMON_TYPES);
+
+  return {
+    loading,
+    error,
+    data: data?.pokemon_v2_type
+      .map(({ name }) => name)
+      .filter((value) => value !== "unknown"),
+  };
 };
 
 export default usePokemonTypes;
