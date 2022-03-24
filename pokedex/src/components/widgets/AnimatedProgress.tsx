46f484c77d06a4e99ac1accb71e073eaf27afc38
jp calvo <calvojp92@gmail.com>
2022-03-24 10:55:38 +0800

    new component

---
+++ pokedex/src/components/widgets/AnimatedProgress.tsx
@@ -0,0 +1,19 @@
+import { Progress, ProgressProps } from "@chakra-ui/react";
+import * as React from "react";
+
+type AnimatedProgressProps = ProgressProps;
+
+const AnimatedProgress = ({
+  value: maxValue,
+  ...props
+}: AnimatedProgressProps) => {
+  const [value, setValue] = React.useState(0);
+
+  React.useEffect(() => {
+    if (maxValue && value < maxValue) setValue((old) => old + 1);
+  }, [maxValue, value]);
+
+  return <Progress value={value} {...props} />;
+};
+
+export default AnimatedProgress;
