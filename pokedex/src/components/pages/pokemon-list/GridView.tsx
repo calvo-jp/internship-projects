9e4e32cf5ff9b9b03ca7a7cd14e8d341dcb3aabe
jp calvo <calvojp92@gmail.com>
2022-03-24 18:26:23 +0800

    separated

---
+++ pokedex/src/components/pages/pokemon-list/GridView.tsx
@@ -0,0 +1,40 @@
+import { SimpleGrid, Spinner } from "@chakra-ui/react";
+import Thumbnail from "components/widgets/Thumbnail";
+import Link from "next/link";
+import getPokemonImageUrl from "utils/getPokemonImageUrl";
+import { GetPokemons } from "__generated__/GetPokemons";
+
+type Pokemon = GetPokemons["pokemons"][number];
+
+interface GridViewProps {
+  data: Pokemon[];
+}
+
+const GridView = ({ data }: GridViewProps) => {
+  return (
+    <SimpleGrid columns={{ md: 2, lg: 4 }} gap={{ base: 4, lg: 8 }}>
+      {data.map((pokemon) => (
+        <GridViewItem key={pokemon.id} data={pokemon} />
+      ))}
+    </SimpleGrid>
+  );
+};
+
+interface GridViewItemProps {
+  data: Pokemon;
+}
+
+const GridViewItem = ({ data }: GridViewItemProps) => {
+  return (
+    <Link href={"/pokemons/" + data.id} passHref>
+      <Thumbnail
+        as="a"
+        h="260px"
+        src={getPokemonImageUrl(data.id)}
+        loader={<Spinner size="xl" />}
+      />
+    </Link>
+  );
+};
+
+export default GridView;
