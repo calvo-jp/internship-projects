0f984075317c958c6ec78917970957d1b3b91d06
jp calvo <calvojp92@gmail.com>
2022-03-25 05:29:08 +0800

    updates

--- pokedex/src/hooks/useNavigate.ts
+++ pokedex/src/hooks/useNavigate.ts
@@ -1,15 +1,18 @@
 import { useRouter } from "next/router";
 import cleanValues from "utils/cleanValues";
 
-const useNavigate = (shallow?: boolean) => {
+interface Config {
+  scroll?: boolean;
+  shallow?: boolean;
+}
+
+const useNavigate = (config?: Config) => {
   const router = useRouter();
 
   return (url: string, query?: Record<string, any>) => {
     const search = new URLSearchParams(cleanValues(query));
 
-    router.push([url, search.toString()].join("?"), undefined, {
-      shallow,
-    });
+    router.push([url, search.toString()].join("?"), undefined, config);
   };
 };
 
