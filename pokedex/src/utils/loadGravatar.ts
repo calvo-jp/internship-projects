bfafac0bd46890a205bfa6eceb69ef2e5ebc7443
jp calvo <calvojp92@gmail.com>
2022-03-25 08:41:34 +0800

    moved to parent

--- pokedex/src/utils/loadGravatar.ts
+++ pokedex/src/utils/loadGravatar.ts
@@ -1,7 +1,16 @@
-import sha256 from "./sha256";
-
 const loadGravatar = (email: string) => {
   return "https://www.gravatar.com/avatar/" + sha256(email) + "?d=retro&s=150";
+};
+
+const sha256 = async (text: string) => {
+  const utf8 = new TextEncoder().encode(text);
+  const hashBuffer = await crypto.subtle.digest("SHA-256", utf8);
+  const hashArray = Array.from(new Uint8Array(hashBuffer));
+  const hashHex = hashArray
+    .map((bytes) => bytes.toString(16).padStart(2, "0"))
+    .join("");
+
+  return hashHex;
 };
 
 export default loadGravatar;
