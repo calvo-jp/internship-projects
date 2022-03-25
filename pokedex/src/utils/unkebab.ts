6075de09ef854beae3e6f0698f58fc42b22f2b05
jp calvo <calvojp92@gmail.com>
2022-03-25 08:42:01 +0800

    added jsdoc

--- pokedex/src/utils/unkebab.ts
+++ pokedex/src/utils/unkebab.ts
@@ -1,3 +1,12 @@
+/**
+ *
+ * Converts kebab case to normal strings
+ *
+ * @example
+ * const str = unkebab("kebab-cased")
+ * console.log(str) // kebab case
+ *
+ */
 const unkebab = (subject: string) => {
   return subject.replace(/\-/g, " ");
 };
