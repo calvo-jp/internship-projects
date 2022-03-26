ca7c044b2960a5f7359f148b16dee0bfb3eb1463
jp calvo <calvojp92@gmail.com>
2022-03-26 21:32:49 +0800

    manually handling fallback since chakra does not handle errors efficiently and still shows broken image

--- pokedex/src/components/widgets/Photo.tsx
+++ pokedex/src/components/widgets/Photo.tsx
@@ -9,12 +9,20 @@ interface ImageProps {
   src: string;
   /** Displayed while the image is still loading */
   loader?: JSX.Element | string;
+  /** fallback src */
+  fallback?: string;
 }
 
 const previouslyLoadedImage: string[] = [];
 
-const Photo = ({ src, loader, ...props }: ImageProps & ChakraImageProps) => {
+const Photo = ({
+  src,
+  loader,
+  fallback,
+  ...props
+}: ImageProps & Omit<ChakraImageProps, "fallback" | "fallbackSrc">) => {
   const [loading, setLoading] = React.useState(true);
+  const [error, setError] = React.useState(false);
 
   React.useEffect(() => {
     if (previouslyLoadedImage.includes(src)) return setLoading(false);
@@ -26,16 +34,26 @@ const Photo = ({ src, loader, ...props }: ImageProps & ChakraImageProps) => {
       setLoading(false);
     };
 
+    image.onerror = () => {
+      setError(true);
+      setLoading(false);
+    };
+
     return () => {
+      setError(false);
       setLoading(true);
+
       image.onload = null;
+      image.onerror = null;
     };
   }, [src]);
 
   return (
     <React.Fragment>
       {!!loading && loader}
-      {!loading && <ChakraImage src={src} loading="lazy" {...props} />}
+      {!loading && (
+        <ChakraImage src={error ? fallback : src} loading="lazy" {...props} />
+      )}
     </React.Fragment>
   );
 };
