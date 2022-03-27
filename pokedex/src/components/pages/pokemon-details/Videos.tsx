ce27c3c759f3e4e1cc2ec5d8babc1d3cb46721a4
jp calvo <calvojp92@gmail.com>
2022-03-28 00:06:09 +0800

    updates

--- pokedex/src/components/pages/pokemon-details/Videos.tsx
+++ pokedex/src/components/pages/pokemon-details/Videos.tsx
@@ -1,4 +1,5 @@
-import { AspectRatio, Box, Flex, SimpleGrid, Text } from "@chakra-ui/react";
+import { AspectRatio, Box, SimpleGrid } from "@chakra-ui/react";
+import Alert from "components/widgets/Alert";
 import Photo from "components/widgets/Photo";
 import * as React from "react";
 import services from "services";
@@ -11,41 +12,56 @@ type YoutubeResult = Awaited<ReturnType<typeof services.youtube.search>>;
 
 const Videos = ({ search }: VideosProps) => {
   const [data, setData] = React.useState<YoutubeResult>();
+  const [error, setError] = React.useState(false);
   const [loading, setLoading] = React.useState(false);
 
-  // const performSearch = React.useCallback(async () => {
-  //   try {
-  //     const response = await services.youtube.search(search, "pokemon");
-  //     setData(response);
-  //   } catch (error) {
-  //     console.log(error);
-  //   }
-  // }, [search]);
+  const performSearch = React.useCallback(async () => {
+    setLoading(true);
 
-  // React.useEffect(() => {
-  //   performSearch();
-  // }, [performSearch]);
+    try {
+      const response = await services.youtube.search(search, "pokemon");
+      setData(response);
+    } catch (error) {
+      setError(true);
+    } finally {
+      setLoading(false);
+    }
+  }, [search]);
+
+  const handleClose = () => setError(false);
+
+  React.useEffect(() => {
+    performSearch();
+  }, [performSearch]);
 
   return (
-    <SimpleGrid columns={{ lg: 2 }} gap={4}>
-      {Array(10)
-        .fill(null)
-        .map((_, idx) => (
-          <Item key={idx} />
+    <Box>
+      <Alert
+        mb={4}
+        open={error}
+        variant="error"
+        message="Something went wrong"
+        onClose={handleClose}
+      />
+
+      <SimpleGrid columns={{ lg: 2 }} gap={4}>
+        {data?.items.map((item) => (
+          <Item key={item.id.videoId} data={item} />
         ))}
-    </SimpleGrid>
+      </SimpleGrid>
+    </Box>
   );
 };
 
 interface ItemProps {
-  data?: YoutubeResult["items"][number];
+  data: YoutubeResult["items"][number];
 }
 
 const Item = ({ data }: ItemProps) => {
   return (
     <AspectRatio>
       <iframe
-        src="https://www.youtube.com/embed/r0yq_yXFQhg"
+        src={"https://www.youtube.com/embed/" + data.id.videoId}
         title="YouTube video player"
         frameBorder={0}
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
