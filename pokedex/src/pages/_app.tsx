f19d474e4e949141f2c9d037af7f2bc140800182
jp calvo <calvojp92@gmail.com>
2022-03-28 17:31:35 +0800

    switching to cdn fonts

--- pokedex/src/pages/_app.tsx
+++ pokedex/src/pages/_app.tsx
@@ -1,15 +1,5 @@
 import { ApolloProvider } from "@apollo/client";
 import { ChakraProvider } from "@chakra-ui/react";
-import "@fontsource/inter/100.css";
-import "@fontsource/inter/200.css";
-import "@fontsource/inter/300.css";
-import "@fontsource/inter/400.css";
-import "@fontsource/inter/500.css";
-import "@fontsource/inter/600.css";
-import "@fontsource/inter/700.css";
-import "@fontsource/inter/800.css";
-import "@fontsource/inter/900.css";
-import "@fontsource/public-sans/400.css";
 import Lightbox from "components/lightbox";
 import PageLoader from "components/PageLoader";
 import apolloClient from "config/apollo/client";
--- pokedex/src/pages/_document.tsx
+++ pokedex/src/pages/_document.tsx
@@ -8,6 +8,16 @@ class MyDocument extends Document {
       <Html>
         <Head>
           <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link
+            rel="preconnect"
+            href="https://fonts.gstatic.com"
+            crossOrigin=""
+          />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&family=Public+Sans&display=swap"
+            rel="stylesheet"
+          />
         </Head>
 
         <body>
