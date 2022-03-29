9efb3c74cb15e23a5f7d184629612d804c9e75aa
jp calvo <calvojp92@gmail.com>
2022-03-29 10:03:10 +0800

    will continue working once quota is reset

--- pokedex/src/components/pages/pokemon-details/Videos.tsx
+++ pokedex/src/components/pages/pokemon-details/Videos.tsx
@@ -1,4 +1,11 @@
-import { AspectRatio, Box, SimpleGrid } from "@chakra-ui/react";
+import {
+  AspectRatio,
+  Box,
+  Center,
+  SimpleGrid,
+  Spinner,
+} from "@chakra-ui/react";
+import InfiniteScroll from "components/InfiniteScroll";
 import Alert from "components/widgets/Alert";
 import Photo from "components/widgets/Photo";
 import * as React from "react";
@@ -13,20 +20,39 @@ type YoutubeResult = Awaited<ReturnType<typeof services.youtube.search>>;
 const Videos = ({ search }: VideosProps) => {
   const [data, setData] = React.useState<YoutubeResult>();
   const [error, setError] = React.useState(false);
-  const [loading, setLoading] = React.useState(false);
+  const [loading, setLoading] = React.useState(true);
 
   const performSearch = React.useCallback(async () => {
     setLoading(true);
 
     try {
-      const response = await services.youtube.search(search, "pokemon");
-      setData(response);
+      const response = await services.youtube.search({
+        query: [search, "pokemon"],
+        token: data?.nextPageToken,
+      });
+
+      setData((current) => {
+        if (!current) return response;
+
+        return {
+          etag: response.etag,
+          kind: response.kind,
+          items: [...current.items, ...response.items],
+          pageInfo: response.pageInfo,
+          regionCode: response.regionCode,
+          nextPageToken: response.nextPageToken,
+          previousPageToken: response.previousPageToken,
+        };
+      });
     } catch (error) {
       setError(true);
     } finally {
       setLoading(false);
     }
-  }, [search]);
+
+    // This causes re-renders all the time
+    // needs refactoring or rewriting of codes
+  }, [data?.nextPageToken, search]);
 
   const handleClose = () => setError(false);
 
@@ -34,44 +60,36 @@ const Videos = ({ search }: VideosProps) => {
     performSearch();
   }, [performSearch]);
 
+  const hasNext = data && data.nextPageToken;
+
   return (
     <Box>
-      <Alert
-        mb={4}
-        open={false}
-        variant="error"
-        message="Something went wrong"
-        onClose={handleClose}
-      />
-
       <SimpleGrid columns={{ lg: 2 }} gap={4}>
         {data?.items.map((item) => (
           <Item key={item.id.videoId} data={item} />
         ))}
-
-        {[
-          "Cw3_PdcL-wY",
-          "qqQeR7sVtlE",
-          "_h6zCYuBdyI",
-          "j9axz6mcalo",
-          "dYagFAay2eU",
-        ].map((item) => (
-          <Item id={item} key={item} />
-        ))}
       </SimpleGrid>
+
+      {loading && (
+        <Center>
+          <Spinner size="lg" />
+        </Center>
+      )}
+
+      <InfiniteScroll callback={performSearch} paused={loading || !hasNext} />
     </Box>
   );
 };
 
-type ItemProps = { data: YoutubeResult["items"][number] } | { id: string };
+interface ItemProps {
+  data: YoutubeResult["items"][number];
+}
 
-const Item = (props: ItemProps) => {
-  const videoId = "id" in props ? props.id : props.data.id.videoId;
-
+const Item = ({ data }: ItemProps) => {
   return (
     <AspectRatio border="1px" borderColor="brand.gray.800" shadow="md">
       <iframe
-        src={"https://www.youtube.com/embed/" + videoId}
+        src={"https://www.youtube.com/embed/" + data.id.videoId}
         title="YouTube video player"
         frameBorder={0}
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
