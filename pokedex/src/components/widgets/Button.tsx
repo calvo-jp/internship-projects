df8f7f23518c6f29e277cea9a689a8da33c76333
jp calvo <calvojp92@gmail.com>
2022-03-29 16:23:07 +0800

    removed jsdoc

--- pokedex/src/components/widgets/Button.tsx
+++ pokedex/src/components/widgets/Button.tsx
@@ -1,12 +1,5 @@
 import { Button as ChakraButton, ButtonProps } from "@chakra-ui/react";
 
-/**
- *
- * Abstracted Button component with default `bgColor` and `color`
- *
- * @example
- * <Button type="submit" mt={4}>Login</Button>
- */
 const Button = ({ children, ...props }: ButtonProps) => {
   return (
     <ChakraButton color="brand.gray.800" bgColor="brand.primary" {...props}>
