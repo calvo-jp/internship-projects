c3f068777e30355495b9b8c8b0935e05b4be62ae
jp calvo <calvojp92@gmail.com>
2022-03-29 13:53:12 +0800

    more sensible response type

--- pokedex/src/hooks/useFetch.ts
+++ pokedex/src/hooks/useFetch.ts
@@ -1,5 +1,24 @@
 import * as React from "react";
 
+// typescript does not know how to handle this
+// type checking should be bypassed in return stmt
+type FetchStatus =
+  | {
+      loading: true;
+      error: undefined;
+      data: undefined;
+    }
+  | {
+      loading: false;
+      error: undefined;
+      data: true;
+    }
+  | {
+      loading: false;
+      error: true;
+      data: undefined;
+    };
+
 /**
  *
  * Fetch as hook
@@ -7,7 +26,10 @@ import * as React from "react";
  * _only supports json_
  *
  */
-const useFetch = <T = any, E = any>(input: RequestInfo, init?: RequestInit) => {
+const useFetch = <T = any, E = any>(
+  input: RequestInfo,
+  init?: RequestInit
+): FetchStatus => {
   const [loading, setLoading] = React.useState(true);
   const [error, setError] = React.useState<E>();
   const [data, setData] = React.useState<T | null>(null);
@@ -20,11 +42,8 @@ const useFetch = <T = any, E = any>(input: RequestInfo, init?: RequestInit) => {
       .finally(() => setLoading(false));
   }, [init, input]);
 
-  return {
-    loading,
-    error,
-    data,
-  };
+  // @ts-expect-error
+  return { loading, error, data };
 };
 
 export default useFetch;
