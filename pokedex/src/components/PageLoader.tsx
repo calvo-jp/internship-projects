18416e9c5a8d108910e114874834cfac511475f4
jp calvo <calvojp92@gmail.com>
2022-03-30 16:00:14 +0800

    added cleanup

--- pokedex/src/components/PageLoader.tsx
+++ pokedex/src/components/PageLoader.tsx
@@ -22,6 +22,12 @@ const PageLoader = () => {
     router.events.on("routeChangeStart", handleStart);
     router.events.on("routeChangeComplete", handleComplete);
     router.events.on("routeChangeError", handleComplete);
+
+    return () => {
+      router.events.off("routeChangeStart", handleStart);
+      router.events.off("routeChangeComplete", handleComplete);
+      router.events.off("routeChangeError", handleComplete);
+    };
   }, [router.events]);
 
   // hide loader when generating new pages or on error
