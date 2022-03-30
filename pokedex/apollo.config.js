f124c86359e43b7e42bbbdde9dbaf9c147b316cf
jp calvo <calvojp92@gmail.com>
2022-03-30 19:58:06 +0800

    added info comment

--- pokedex/apollo.config.js
+++ pokedex/apollo.config.js
@@ -1,3 +1,9 @@
+// HEADS UP!
+//
+// Apollo vscode extension can only handle one endpoint.
+// Queries, mutations and subs will error out
+// depending on what endpoint is currently not used.
+
 const endpoints = {
   main: "https://beta.pokeapi.co/graphql/v1beta",
   auth: "https://frontend-engineer-onboarding-api-thxaa.ondigitalocean.app/graphql",
