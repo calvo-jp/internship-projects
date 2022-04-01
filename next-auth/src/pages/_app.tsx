8d72fc010bc599db36320af26750916f028212d5
jp calvo <calvojp92@gmail.com>
2022-04-01 09:11:31 +0800

    switched to cdn fonts

--- next-auth/src/pages/_app.tsx
+++ next-auth/src/pages/_app.tsx
@@ -1,16 +1,5 @@
 import { ApolloProvider } from "@apollo/client";
 import { ChakraProvider } from "@chakra-ui/react";
-import "@fontsource/montserrat/100.css";
-import "@fontsource/montserrat/200.css";
-import "@fontsource/montserrat/300.css";
-import "@fontsource/montserrat/400.css";
-import "@fontsource/montserrat/500.css";
-import "@fontsource/montserrat/600.css";
-import "@fontsource/montserrat/700.css";
-import "@fontsource/ubuntu/300.css";
-import "@fontsource/ubuntu/400.css";
-import "@fontsource/ubuntu/500.css";
-import "@fontsource/ubuntu/700.css";
 import apolloClient from "config/apollo/client";
 import theme from "config/theme";
 import { SessionProvider } from "next-auth/react";
